5755cd649e09966bf3f9c8a77599fbe29223c25e
Unknown <yummytea1@gmail.com>
2017-11-14 10:48:28 +0100

    BulkAusgabe changes
    Handling of multiple existing instances. 
    + other stuff

--- DBentry/views.py
+++ DBentry/views.py
@@ -196,18 +196,75 @@ class MIZAdminView(FormView):
         # which we want on all our views
         return render(request, self.template_name, context = self.get_context_data(**kwargs))
         
+#
+#class BulkBase(MIZAdminView):
+#    template_name = 'admin/bulk.html'
+#    form_class = None
+#    success_url = ''
+#    
+#    def post(self, request, *args, **kwargs):
+#        context = self.get_context_data(**kwargs)
+#        form = self.form_class(request.POST, initial=request.session.get('old_form_data', {}))
+#        if form.is_valid():
+#            if form.has_changed() or '_preview' in request.POST:
+#                if not '_preview' in request.POST:
+#                    messages.warning(request, 'Angaben haben sich geändert. Bitte kontrolliere diese in der Vorschau.')
+#                context['preview_headers'], context['preview'] = self.build_preview(request, form)
+#            else:
+#                if '_continue' in request.POST and not form.has_changed():
+#                    # Collect preview data, create instances
+#                    ids, instances = self.save_data(request, form)
+#                    # Need to store the queryset of the newly created items in request.session for the Changelist view
+#                    request.session['qs'] = dict(id__in=ids) if ids else None
+#                    return redirect(self.success_url)
+#                if '_addanother' in request.POST and not form.has_changed():
+#                    old_form = form
+#                    ids, created, updated = self.save_data(request, form)
+#                    from django.core.urlresolvers import reverse
+#                    if created:
+#                        obj_list = link_list(request, created, path = "admin:DBentry_ausgabe_change")
+#                        messages.success(request, format_html('Ausgaben erstellt: {}'.format(obj_list)))
+#                    if updated:
+#                        obj_list = link_list(request, updated, path = "admin:DBentry_ausgabe_change")
+#                        messages.success(request, format_html('Dubletten hinzugefügt: {}'.format(obj_list)))
+#                    
+#                    form = self.form_class(self.next_initial_data(form))
+#                    form.is_valid()
+#                    context['preview_headers'], context['preview'] = self.build_preview(request, form)
+#        request.session['old_form_data'] = form.data
+#        context['form'] = form
+#        return render(request, self.template_name, context = context)
+#    
+#    def next_initial_data(self, form):
+#        return form.data
+#    
+#    def save_data(self, request, form):
+#        return []
+#        
+#    def build_preview(self, request, form):
+#        return 
+#        
+#    def instance_data(self, row):
+#        return {}
+#        #TODO: this is WIP
+#        for fld_name in form.each_fields:
+#            if fld_name in form.split_data:
+#                continue
+#                rslt[fld_name] = form.split_data.get(fld_name)
+#            else:
+#                rslt[fld_name] = form.cleaned_data.get(fld_name)
+#        return rslt
 
-class BulkBase(MIZAdminView):
+class BulkAusgabe(MIZAdminView):
     template_name = 'admin/bulk.html'
-    form_class = None
-    success_url = ''
+    form_class = BulkFormAusgabe
+    success_url = 'admin:DBentry_ausgabe_changelist'
     
     def post(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         form = self.form_class(request.POST, initial=request.session.get('old_form_data', {}))
-        
         if form.is_valid():
-            if form.has_changed():
+            if form.has_changed() or '_preview' in request.POST:
                 if not '_preview' in request.POST:
                     messages.warning(request, 'Angaben haben sich geändert. Bitte kontrolliere diese in der Vorschau.')
                 context['preview_headers'], context['preview'] = self.build_preview(request, form)
@@ -220,9 +277,15 @@ class BulkBase(MIZAdminView):
                     return redirect(self.success_url)
                 if '_addanother' in request.POST and not form.has_changed():
                     old_form = form
-                    ids, instances = self.save_data(request, form)
-                    obj_list = link_list(request, instances)
-                    messages.success(request, format_html('Ausgaben erstellt: {}'.format(obj_list)))
+                    ids, created, updated = self.save_data(request, form)
+                    from django.core.urlresolvers import reverse
+                    if created:
+                        obj_list = link_list(request, created, path = "admin:DBentry_ausgabe_change")
+                        messages.success(request, format_html('Ausgaben erstellt: {}'.format(obj_list)))
+                    if updated:
+                        obj_list = link_list(request, updated, path = "admin:DBentry_ausgabe_change")
+                        messages.success(request, format_html('Dubletten hinzugefügt: {}'.format(obj_list)))
+                    
                     form = self.form_class(self.next_initial_data(form))
                     form.is_valid()
                     context['preview_headers'], context['preview'] = self.build_preview(request, form)
@@ -230,40 +293,48 @@ class BulkBase(MIZAdminView):
         context['form'] = form
         return render(request, self.template_name, context = context)
     
-    def next_initial_data(self, form):
-        return form.data
-    
-    def save_data(self, request, form):
-        return []
-        
-    def build_preview(self, request, form):
-        return 
-        
-    def instance_data(self, row):
-        return {}
-        #TODO: this is WIP
-        for fld_name in form.each_fields:
-            if fld_name in form.split_data:
-                continue
-                rslt[fld_name] = form.split_data.get(fld_name)
-            else:
-                rslt[fld_name] = form.cleaned_data.get(fld_name)
-        return rslt
-
-class BulkAusgabe(BulkBase):
-    form_class = BulkFormAusgabe
-    success_url = 'admin:DBentry_ausgabe_changelist'
-    
     def save_data(self, request, form):
         ids = []
-        instances = []
-        row_count = len(form.row_data)
+        created = []
+        updated = []
         
+        original = []
+        dupes = []
+        # Split row_data into rows of duplicates and originals, so we save the originals before any duplicates
+        # This assumes row_data does not contain any nested duplicates
+        # Also filter out rows that resulted in multiple matching existing instances
         for row in form.row_data:
+            if 'multiples' in row:
+                continue
+            if 'dupe_of' in row:
+                dupes.append(row)
+            else:
+                original.append(row)
+                
+        from itertools import chain
+        
+        for row in chain(original, dupes):
+            if 'dupe_of' in row:
+                instance = row['dupe_of'].get('instance', None)
+                if not instance or not instance.pk:
+                    # NOTE: can this still happpen?
+                    raise ValidationError("Hol mal den Philip!")
+                    continue
+                bestand_data = dict(lagerort=row.get('lagerort'))
+                if 'provenienz' in row['dupe_of']:
+                    bestand_data['provenienz'] = row.get('provenienz')
+                instance.bestand_set.create(**bestand_data)
+                row['instance'] = instance
+                updated.append(instance)
+                ids.append(instance.pk)
+                continue
+            
             instance = row.get('instance', None) or ausgabe(**self.instance_data(row)) 
             if not instance.pk:
                 instance.save()
-            instances.append(instance)
+                created.append(instance)
+            else:
+                updated.append(instance)
             for fld_name in ['jahr', 'num', 'monat', 'lnum']:
                 set = getattr(instance, "ausgabe_{}_set".format(fld_name))
                 data = row.get(fld_name, None)
@@ -293,9 +364,8 @@ class BulkAusgabe(BulkBase):
                 else:
                     audio_instance = audio(**audio_data)
                     audio_instance.save()
-                # set.add/ set.create didnt work for some fcking reason
-                ergh = m2m_audio_ausgabe(ausgabe=instance, audio=audio_instance)
-                ergh.save()
+                m2m_instance = m2m_audio_ausgabe(ausgabe=instance, audio=audio_instance)
+                m2m_instance.save()
                 bestand_data = dict(lagerort=form.cleaned_data.get('audio_lagerort'))
                 if 'provenienz' in row:
                     bestand_data['provenienz'] = row.get('provenienz')
@@ -307,8 +377,9 @@ class BulkAusgabe(BulkBase):
                 bestand_data['provenienz'] = row.get('provenienz')
             instance.bestand_set.create(**bestand_data)
             
+            row['instance'] = instance
             ids.append(instance.pk)
-        return ids, instances
+        return ids, created, updated
                 
     def next_initial_data(self, form):
         data = form.data.copy()
@@ -330,46 +401,70 @@ class BulkAusgabe(BulkBase):
         from django.contrib.admin.utils import reverse
         from django.utils.html import format_html
         preview_data = []
+        headers = []
+        preview_fields = []
+        row_error = False
+        multiple_instances_msg = "Es wurden mehrere passende Ausgaben gefunden. Es soll immer nur eine bereits bestehende Ausgabe verändert werden: diese Zeile wird ignoriert. "
+        one_instance_msg = "Es wird ein Dubletten-Bestand zu dieser Ausgabe hinzugefügt."
         
         for row in form.row_data:
             preview_row = OrderedDict()
             for fld_name in form.preview_fields:
-                if not row.get(fld_name):
+                if not fld_name in row:
                     continue
                 if fld_name == 'audio':
                     if row.get(fld_name):
                         img = format_html('<img alt="True" src="/static/admin/img/icon-yes.svg">')
                     else:
+                        # NOTE: this never happens...
                         img = format_html('<img alt="False" src="/static/admin/img/icon-no.svg">')
                     preview_row[fld_name] = img
-                    continue
-                values_list = row.get(fld_name) or []
-                if isinstance(values_list, list):
-                    if len(values_list)==1:
-                        preview_row[fld_name] = values_list[0] or ''
-                    else:
-                        preview_row[fld_name] = ", ".join(values_list)
                 else:
-                    preview_row[fld_name] = values_list or ''
-        
-            preview_row['Instanz'] = row.get('instance', '') or ''
-            if preview_row['Instanz']:
-                link = reverse("admin:DBentry_ausgabe_change", args = [preview_row['Instanz'].pk])
-                label = str(preview_row['Instanz'])
-                img = format_html('<img alt="False" src="/static/admin/img/icon-alert.svg">')
-                preview_row['Instanz'] = format_html('{} <a href="{}" target="_blank">{}</a>', img, link, label)
+                    values_list = row.get(fld_name) or []
+                    if isinstance(values_list, list):
+                        if len(values_list)==1:
+                            preview_row[fld_name] = values_list[0] or ''
+                        else:
+                            preview_row[fld_name] = ", ".join(values_list)
+                    else:
+                        preview_row[fld_name] = values_list or ''
+                preview_fields.append(fld_name) # record the field's appearance for the headers creation    
+                
+            if 'instance' in row:
+                instances = [row.get('instance')]
+            else:
+                instances = list(row.get('multiples', []))
+                
+            if len(instances)==0:
+                preview_row['Instanz'] = '---'
+            else:
+                links = []
+                for instance in instances:
+                    link = reverse("admin:DBentry_ausgabe_change", args = [instance.pk])
+                    label = str(instance)
+                    links.append(format_html('<a href="{}" target="_blank">{}</a>', link, label))
+                    
+                if len(instances)==1:
+                    img = '<img alt="False" src="/static/admin/img/icon-alert.svg">'
+                    preview_row['Instanz'] = format_html(img + ", ".join(links))
+                    preview_row['Bemerkung'] = one_instance_msg
+                else:
+                    row_error = True
+                    img = '<img alt="False" src="/static/admin/img/icon-no.svg">'
+                    preview_row['Instanz'] = format_html(img + ", ".join(links))
+                    preview_row['Bemerkung'] = multiple_instances_msg
+                    
             preview_data.append(preview_row)
             
-        headers = []
         for fld_name in form.preview_fields:
-            if not any(fld_name in row for row in preview_data):
-                # Do not include headers for fields that are not in the preview_data
+            if fld_name not in preview_fields:
+                # This field does not appear at least once in preview_data
                 continue
-            if fld_name == 'lagerort':
-                headers.append('Lagerort')
-            elif form.fields.get(fld_name).label:
+            if form.fields.get(fld_name).label:
                 headers.append(form.fields.get(fld_name).label.strip(":"))
             else:
                 headers.append(fld_name)
         headers += ['Bereits vorhanden']
+        if row_error:
+            headers += ['Bemerkung']
         return headers, preview_data
