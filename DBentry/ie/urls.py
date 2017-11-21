7876ec921eefa2760edeb7d48c18c9f9d972e68c
Unknown <yummytea1@gmail.com>
2017-11-21 12:10:47 +0100

    url pattern rework

--- DBentry/ie/urls.py
+++ DBentry/ie/urls.py
@@ -2,7 +2,6 @@
 from django.conf.urls import url
 from .views import ImportSelectView
 
-import_urls = [
+urlpatterns = [
         url(r'^discogs_import/$',ImportSelectView.as_view(), name='import_select'),
     ]
-
--- DBentry/urls.py
+++ DBentry/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url,  include
 
-urlpatterns = [
-    url(r'',  include('DBentry.ac.urls'))
-]
 
 from .views import BulkAusgabe
+
 admin_tools_urls = [
     url(r'^bulk_ausgabe/$', BulkAusgabe.as_view(), name='bulk_ausgabe'), 
+    url(r'', include('DBentry.ie.urls'))
+]
+
+urlpatterns = [
+    url(r'^ac/',  include('DBentry.ac.urls')), 
+    url(r'^tools/', include(admin_tools_urls))
 ]
