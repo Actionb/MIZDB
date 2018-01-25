0a373e80963fafac12bd9c3a35328d3bad82e60f
Unknown <yummytea1@gmail.com>
2018-01-25 14:15:38 +0100

    AdminActions v0.0001
    Still lots to do.

--- DBentry/actions.py
+++ DBentry/actions.py
@@ -3,6 +3,8 @@ from django.utils.translation import ugettext as _, ugettext_lazy
 from django.shortcuts import redirect  
 from django.urls import reverse
 
+from .views import MIZAdminMixin, OptionalFormView
+
 def merge_records(model_admin, request, queryset):
     if queryset.count()==1:
         model_admin.message_user(request,'Es müssen mindestens zwei Objekte aus der Liste ausgewählt werden, um diese Aktion durchzuführen', 'warning')
@@ -12,4 +14,102 @@ def merge_records(model_admin, request, queryset):
     request.session['merge'] = {'qs_ids': list(queryset.values_list('pk', flat=True)), 'success_url' : request.get_full_path()}
     return redirect(reverse('merge', kwargs=dict(model_name=model_admin.opts.model_name)))
 merge_records.short_description = ugettext_lazy("Merge selected %(verbose_name_plural)s")#'Datensätze zusammenfügen'
-merge_records.perm_required = 'merge'
+merge_records.perm_required = ['merge']
+
+class ActionConfirmationView(MIZAdminMixin, OptionalFormView):
+    #TODO: text stuff
+    #TODO: put in an actual form for user input
+    
+    template_name = 'admin/action_confirmation.html'
+    queryset = None
+    model_admin = None
+    opts = None
+    action_name = None
+    
+    def __init__(self, *args, **kwargs):
+        # queryset and model_admin are passed in from initkwargs in as_view(cls,**initkwargs).view(request)-> cls(**initkwargs)
+        self.queryset = kwargs.pop('queryset')
+        self.model_admin = kwargs.pop('model_admin')
+        if not getattr(self, 'action_name', False): # Allow setting of custom action_names, otherwise use the class's name
+            self.action_name = self.__class__.__name__
+        self.opts = self.model_admin.opts
+        super(ActionConfirmationView, self).__init__(*args, **kwargs)
+        
+    def form_valid(self, form):
+        # OptionalFormView returns this when either the (optional) form is not given or the form is valid.
+        self.perform_action(None if form is None else form.cleaned_data)
+        
+        # We always want to be redirected back to the changelist the action originated from (request.get_full_path()).
+        # If we return None, options.ModelAdmin.response_action will do the redirect for us.
+        return None
+        
+    def action_allowed(self):
+        return True
+        
+    def perform_action(self, form_cleaned_data = None):
+        raise NotImplementedError('Subclasses must implement this method.')
+        
+    def compile_affected_objects(self):
+        #TODO: link_list this // NestedObjects stuff -> needs to show relevant data (jg,bestand,etc.)
+        objs = []
+        for obj in self.queryset:
+            objs.append([str(obj)])
+        return objs
+    
+    def post(self, request, *args, **kwargs):
+        if request.POST.get('action_confirmed', False):
+            # User has confirmed the action  --- OptionalFormView will figure out the correct response to give.
+            return super(ActionConfirmationView, self).post(request, *args, **kwargs)
+        elif request.POST.get('action_aborted', False):
+            # User aborted the action, redirect back to the changelist
+            return None
+        else:
+            if self.action_allowed():
+                # Display the confirmation page
+                return self.render_to_response(self.get_context_data(*args, **kwargs))    
+            else:
+                # The action was not allowed
+                return None
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ActionConfirmationView, self).get_context_data(*args, **kwargs)
+        
+        if len(self.queryset) == 1:
+            objects_name = force_text(self.opts.verbose_name)
+        else:
+            objects_name = force_text(self.opts.verbose_name_plural)
+        
+        from django.contrib.admin import helpers
+        context.update(
+            dict(
+                title                   =   _("Are you sure?"),
+                objects_name            =   objects_name,
+                queryset                =   self.queryset,
+                affected_objects        =   self.compile_affected_objects(), 
+                opts                    =   self.opts,
+                action_checkbox_name    =   helpers.ACTION_CHECKBOX_NAME,
+                media                   =   self.model_admin.media, #TODO: form.media?
+                action_name             =   self.action_name, # see below (line contrib.admin.options:1255)
+            )
+        )
+        #NOTE: having to specify action_name so the view does not magically (looking at you javascript) redirect to 
+        # the deletion_confirmation template/view... 
+        # if the action_name is the reason the view finds its way back into the 'right' post method (i.e. here NOT in deletion_confirmation)
+        # does that mean I need a view per action anyway - making CBVs useless for this?
+        return context
+        
+class BulkJahrgang(ActionConfirmationView):
+    
+    short_description = 'Jahrgang hinzufügen'
+    perm_required = ['change']
+    
+    def action_allowed(self):
+        if self.queryset.values('magazin_id').distinct().count() != 1:
+            msg_text = "Aktion abgebrochen: ausgewählte Ausgaben stammen von mehr als einem Magazin."
+            self.model_admin.message_user(self.request, msg_text, 'error')
+            return False
+        return True
+    
+    def perform_action(self, form_cleaned_data):
+        self.queryset.order_by().update(jahrgang=form_cleaned_data['jahrgang'])
+
--- DBentry/admin.py
+++ DBentry/admin.py
@@ -11,7 +11,7 @@ from .models import *
 from .forms import makeForm, InLineAusgabeForm
 from .utils import link_list
 from .changelist import MIZChangeList
-from .actions import merge_records
+from .actions import ActionConfirmationView, merge_records, BulkJahrgang
 
 from .sites import miz_site
 
@@ -30,7 +30,83 @@ class ModelBase(admin.ModelAdmin):
     collapse_all = False                    # Whether to collapse all inlines/fieldsets by default or not
     hint = ''                               # A hint displayed at the top of the form 
     actions = [merge_records]
+
+    def response_action(self, request, queryset):
+        """
+        Handle an admin action. This is called if a request is POSTed to the
+        changelist; it returns an HttpResponse if the action was handled, and
+        None otherwise.
+        """
+        #TODO: shit place for imports
+        from django.contrib.admin import helpers
+        import inspect
+        from django.http import HttpResponseRedirect
+        from django.http.response import HttpResponseBase
     
+        # There can be multiple action forms on the page (at the top
+        # and bottom of the change list, for example). Get the action
+        # whose button was pushed.
+        try:
+            action_index = int(request.POST.get('index', 0))
+        except ValueError:
+            action_index = 0
+        # Construct the action form.
+        data = request.POST.copy()
+        data.pop(helpers.ACTION_CHECKBOX_NAME, None)
+        data.pop("index", None)
+
+        # Use the action whose button was pushed
+        try:
+            data.update({'action': data.getlist('action')[action_index]})
+        except IndexError:
+            # If we didn't get an action from the chosen form that's invalid
+            # POST data, so by deleting action it'll fail the validation check
+            # below. So no need to do anything here
+            pass
+
+        action_form = self.action_form(data, auto_id=None)
+        action_form.fields['action'].choices = self.get_action_choices(request)
+
+        # If the form's valid we can handle the action.
+        if action_form.is_valid():
+            action = action_form.cleaned_data['action']
+            select_across = action_form.cleaned_data['select_across']
+            func = self.get_actions(request)[action][0]
+
+            # Get the list of selected PKs. If nothing's selected, we can't
+            # perform an action on it, so bail. Except we want to perform
+            # the action explicitly on all objects.
+            selected = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+            if not selected and not select_across:
+                # Reminder that something needs to be selected or nothing will happen
+                msg = _("Items must be selected in order to perform "
+                        "actions on them. No items have been changed.")
+                self.message_user(request, msg, messages.WARNING)
+                return None
+                
+            if not select_across:
+                # Perform the action only on the selected objects
+                queryset = queryset.filter(pk__in=selected)
+            
+            if inspect.isclass(func) and issubclass(func, ActionConfirmationView):
+                # func is actually CBV, get it's as_view() inner view function 
+                response = func.as_view(model_admin=self, queryset=queryset)(request)
+            else:
+                response = func(self, request, queryset)
+            
+            # Actions may return an HttpResponse-like object, which will be
+            # used as the response from the POST. If not, we'll be a good
+            # little HTTP citizen and redirect back to the changelist page.
+            if isinstance(response, HttpResponseBase):
+                return response
+            else:
+                return HttpResponseRedirect(request.get_full_path())
+        else:
+            msg = _("No action selected.")
+            self.message_user(request, msg, messages.WARNING)
+            return None
+
+
     def has_adv_sf(self):
         return len(getattr(self, 'advanced_search_form', []))>0
     
@@ -393,9 +469,8 @@ class AusgabenAdmin(ModelBase):
                             'nummer' : 'ausgabe_num__num', 
                             'lfd' : 'ausgabe_lnum__lnum', 
                             }
-                        
-    #actions = ['add_duplicate', 'add_bestand', 'merge_records', 'num_to_lnum', 'add_birgit', 'bulk_jg']
-    #actions = ['merge_records']
+                            
+    actions = [BulkJahrgang]
     advanced_search_form = {
         'gtelt':['ausgabe_jahr__jahr', 'ausgabe_num__num', 'ausgabe_lnum__lnum'], 
         'selects':['magazin','status'], 
