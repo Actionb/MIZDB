eaa62e399b66b0581994b7ca1434a47081641be3
Unknown <yummytea1@gmail.com>
2018-10-15 11:37:51 +0200

    Updated object_tools.help_link to use the new registry
    Added the active request to the context of all MIZModelAdmin instances, so we can pass it to the templatetag

--- DBentry/templatetags/object_tools.py
+++ DBentry/templatetags/object_tools.py
@@ -17,33 +17,20 @@ def _get_default_template(is_popup):
 @register.simple_tag   
 def help_link(request, is_popup = False):
     from DBentry.help.registry import halp
-    
-    url_name = ''
-    reverse_args = ()
-    reverse_kwargs = {}
 
-    if not hasattr(request.resolver_match.func, 'model_admin'):
-        # This is not a ModelAdmin view, it's a 'custom' view; get this view's helptext #TODO: THEN WHY ARE WE LOOKING FOR A FORM AND NOT A VIEW?!
-        form_class = request.resolver_match.func.view_class.form_class
-        for _url_name, form_helptext in halp.get_registered_forms().items():
-            if form_helptext.form_class == form_class:
-                url_name = _url_name
-                break
-    else:
+    if hasattr(request.resolver_match.func, 'model_admin'):
         # This is a ModelAdmin view for the add/change page of a certain model
-        model = request.resolver_match.func.model_admin.model
-        if halp.is_registered(model):
-            url_name = 'help'
-            reverse_kwargs = {'model_name': model._meta.model_name}
-    if url_name:
-        try:
-            return format_html(
-                _get_default_template(is_popup), 
-                url = reverse(url_name, args = reverse_args, kwargs = reverse_kwargs), 
-                label = 'Hilfe', 
-            )
-        except exceptions.NoReverseMatch:
-            pass
+        url = halp.help_url_for_view(request.resolver_match.func.model_admin)
+    else:
+        # This is not a ModelAdmin view, it's a 'custom' view; get the helpview's url
+        url = halp.help_url_for_view(request.resolver_match.func.view_class)
+        
+    if url:
+        return format_html(
+            _get_default_template(is_popup), 
+            url = url,
+            label = 'Hilfe', 
+        )
     return ''
     
 @register.simple_tag
