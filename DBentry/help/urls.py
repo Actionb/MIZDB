8150b062b0f287767ebeb52d93c6f27c78b7b32c
Unknown <yummytea1@gmail.com>
2019-02-19 12:47:37 +0100

    ToDo

--- DBentry/help/urls.py
+++ DBentry/help/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url 
 
-
+#TODO: import (or rather prompt the registration) of the models and forms should be done in halp.urls or the like
 # prompt the registration of all model/form helptexts
 from .models import *
 from .forms import *
