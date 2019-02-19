648075d5b24dc9ce1601a45547862533529016ec
Unknown <yummytea1@gmail.com>
2019-02-19 11:30:19 +0100

    Stops fieldsets with errors to start out as collapsed

--- DBentry/static/admin/js/collapse.js
+++ DBentry/static/admin/js/collapse.js
@@ -4,19 +4,22 @@
     $(document).ready(function() {
         // Add anchor tag for Show/Hide
         $("fieldset.collapse").each(function(i, elem) {
+            var has_error = $(elem).find("div.errors").length !== 0;
+            var is_collapsed = $(elem).hasClass('collapsed');
             // Don't hide if fields in this fieldset have errors
-            if ($(elem).find("div.errors").length === 0) {
-                var label_text = gettext("Hide");
-                if($(elem).hasClass("collapsed")){
-                    label_text = gettext("Show");
-                }
-                var hint = '<span class="collapse-hint">' + '(' + label_text + ')' +'</span>';
-                if ($(elem).hasClass("stacked-inline-collapsible")){
-                    $(elem).siblings("h3").first().prepend('<span class="collapse-hint" style="float:left;">' + '(' + label_text + ')' +'</span>')//.children().last().before(hint);
-                }
-                else {
-                    $(elem).find("h2").first().append(hint);
-                }
+            if (has_error && is_collapsed) { 
+                $(elem).removeClass('collapsed'); 
+            }
+            var label_text = gettext("Hide");
+            if($(elem).hasClass("collapsed")){
+                label_text = gettext("Show");
+            }
+            var hint = '<span class="collapse-hint">' + '(' + label_text + ')' +'</span>';
+            if ($(elem).hasClass("stacked-inline-collapsible")){
+                $(elem).siblings("h3").first().prepend('<span class="collapse-hint" style="float:left;">' + '(' + label_text + ')' +'</span>')//.children().last().before(hint);
+            }
+            else {
+                $(elem).find("h2").first().append(hint);
             }
         });
         
@@ -36,20 +39,5 @@
             }
             elem.toggleClass("collapsed");
         });
-        /*
-        // Add toggle to anchor tag
-        $(".collapsible").click(function(ev) {
-            var f = $(this).closest(".collapse");
-            if (f.hasClass("collapsed")) {
-                // Show
-                $(this).children(".collapse-hint").text('('+gettext("Hide")+')');
-                f.removeClass("collapsed").trigger("show.fieldset", [$(this).attr("id")]);
-            } else {
-                // Hide
-                $(this).children(".collapse-hint").text('('+gettext("Show")+')');
-                f.addClass("collapsed").trigger("hide.fieldset", [$(this).attr("id")]);
-            }
-            return false;
-        });*/
     });
 })(django.jQuery);
