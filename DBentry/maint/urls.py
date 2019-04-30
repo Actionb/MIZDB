f74febd575dc748a874c8b749694e541945a321e
Actionb <yummytea1@gmail.com>
2019-04-30 13:32:50 +0200

    ToDo in maint.urls

--- DBentry/maint/urls.py
+++ DBentry/maint/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'dupes/(?P<model_name>\w+)/$' , DuplicateObjectsView.as_view(), name='dupes'), 
     url(r'dupes/$', ModelSelectView.as_view(title='Duplikate', next_view = 'dupes'), name='dupes_select'), 
     url(r'unused/$', ModelSelectView.as_view(title='Unbenutzt', next_view = 'unused_objects'), name='unused_select'), 
-    url(r'', MaintView.as_view(), name='maint_main'),  
+#    url(r'', MaintView.as_view(), name='maint_main'),  #TODO: I assume this is meant to be 'index' of maint
 ] 
