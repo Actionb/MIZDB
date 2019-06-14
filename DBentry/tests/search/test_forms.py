5022d699150025ec37350821d910355074deb510
Actionb <yummytea1@gmail.com>
2019-06-14 13:07:06 +0200

    Updated tests

--- DBentry/tests/search/test_forms.py
+++ DBentry/tests/search/test_forms.py
@@ -46,12 +46,28 @@ class TestSearchFormFactory(MyTestCase):
         self.assertIn('genre__genre', form_class.base_fields)
         self.assertNotIn('notafield', form_class.base_fields)
         self.assertNotIn('schlagwort__notalookup', form_class.base_fields)
-        self.assertEqual(form_class.range_start_suffix, '__gte')
-        self.assertEqual(form_class.range_end_suffix, '__lt')
+        
+    def test_takes_formfield_callback(self):
+        # Assert that custom formfield_callback can be passed to the factory 
+        # and that it uses that to create formfields for dbfields.
+        callback = lambda dbfield: 'This is not a field!'
+        form_class = self.factory(_models.artikel, formfield_callback = callback, fields = ['seite'])
+        self.assertEqual(getattr(form_class, 'seite', None), 'This is not a field!')
+        # A callback that is not a callable should raise a TypeError
+        with self.assertRaises(TypeError):
+            self.factory(_models.artikel, formfield_callback = 1)
+        
+        
     
 class TestSearchForm(MyTestCase):
     
     model = _models.artikel
+    
+    def test_get_filters_params_returns_empty_on_invalid(self):
+        form_class = search_forms.SearchFormFactory()(self.model)
+        form = form_class()
+        # Empty form without data => is_valid == False
+        self.assertFalse(form.get_filter_params())
     
     def test_get_filters_params_skips_empty(self):
         # Assert that get_filter_params does not return empty query values.
@@ -68,14 +84,46 @@ class TestSearchForm(MyTestCase):
         self.assertIn('ausgabe__magazin', filter_params)
         self.assertNotIn('musiker', filter_params)
         
-    def test_get_filters_params_replaces_range_with_exact(self):
-        # Assert that get_filter_params replaces a range query with no end 
-        # with a query for exact.
-        data = {'seite__gte': 1, 'seite__lt': None}
-        form_class =  search_forms.SearchFormFactory()(self.model, fields = data.keys())
+    def test_get_filters_params_range(self):
+        form_class =  search_forms.SearchFormFactory()(self.model, fields = ['seite__range'])
+        data = {'seite__range_0': '1', 'seite__range_1': '2'}
         form = form_class(data = data)
         self.assertTrue(form.is_valid(), msg = form.errors)
         filter_params = form.get_filter_params()
-        self.assertNotIn('seite__lt', filter_params)
-        self.assertNotIn('seite__gte', filter_params)
+        self.assertIn('seite__range', filter_params)
+        self.assertEqual(filter_params['seite__range'], [1, 2])
+        # Little thing: check if a direct lookup of 'seite__range' is possible.
+        data = {'seite__range': ['1', '2']}
+        form = form_class(data = data)
+        self.assertTrue(form.is_valid(), msg = form.errors)
+        filter_params = form.get_filter_params()
+        self.assertIn('seite__range', filter_params)
+        self.assertEqual(filter_params['seite__range'], [1, 2])
+        
+    def test_get_filters_params_range_skipped_when_empty(self):
+        form_class =  search_forms.SearchFormFactory()(self.model, fields = ['seite__range'])
+        data = {'seite__range_0': None, 'seite__range_1': None}
+        form = form_class(data = data)
+        self.assertTrue(form.is_valid(), msg = form.errors)
+        filter_params = form.get_filter_params()
+        self.assertFalse(filter_params)        
+        
+    def test_get_filters_params_replaces_range(self):
+        # Assert that get_filter_params replaces a range query with...
+        # with a query for exact when 'end' is 'empty'
+        form_class =  search_forms.SearchFormFactory()(self.model, fields = ['seite__range'])
+        
+        data = {'seite__range_0': '1', 'seite__range_1': None}
+        form = form_class(data = data)
+        self.assertTrue(form.is_valid(), msg = form.errors)
+        filter_params = form.get_filter_params()
+        self.assertNotIn('seite__range', filter_params)
         self.assertIn('seite', filter_params)
+        
+        # with a query for lte when 'start' is 'empty'
+        data = {'seite__range_0': None, 'seite__range_1': '1'}
+        form = form_class(data = data)
+        self.assertTrue(form.is_valid(), msg = form.errors)
+        filter_params = form.get_filter_params()
+        self.assertNotIn('seite__range', filter_params)
+        self.assertIn('seite__lte', filter_params)
