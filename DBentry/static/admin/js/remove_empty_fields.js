1517e53bf6c1fafd1a354ef7a5ee8c0dfdff4006
Actionb <yummytea1@gmail.com>
2019-06-17 10:02:29 +0200

    Added javascript snippet that removes empty fields from the get request query string

---
+++ DBentry/static/admin/js/remove_empty_fields.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+  
+  // Remove empty fields from GET forms
+  // Author: Bill Erickson
+  // URL: http://www.billerickson.net/code/hide-empty-fields-get-form/
+  	// Change 'form' to class or ID of your specific form
+	$("form").submit(function() {
+        var empty = $(this).find(":input").filter(function(){ return !this.value; });
+		$(this).find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
+		return true; // ensure form still submits
+	});
+	
+	// Un-disable form fields when page loads, in case they click back after submission
+	$( "form" ).find( ":input" ).prop( "disabled", false );
+	
+});
