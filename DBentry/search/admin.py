8a5c2c6fb01c201769e0456f15620a397e46519a
Actionb <yummytea1@gmail.com>
2019-06-28 11:14:39 +0200

    Fixed post save response dropping multiple values from a SelectMultiple filter

--- DBentry/search/admin.py
+++ DBentry/search/admin.py
@@ -1,4 +1,10 @@
+from urllib.parse import parse_qsl, urlparse, urlunparse
+
 from django.contrib.admin.views.main import SEARCH_VAR
+from django.db.models.query import QuerySet
+from django.db.models.constants import LOOKUP_SEP
+from django.http import HttpResponseRedirect, QueryDict
+from django.urls import reverse
 
 from DBentry.search.forms import searchform_factory, MIZAdminSearchForm
 
@@ -116,8 +122,40 @@ class AdminSearchFormMixin(object):
                 if k not in initial:
                     filter_dict[k] = v
         initial.update(filter_dict)
-        return initial
- 
+        return initial       
+        
+    def _response_post_save(self, request, obj):
+        """
+        django's helper method that returns the user back to the changelist (or index if no perms).
+        In its original form, the method uses 
+        django.contrib.admin.templatetags.admin_urls.add_preserved_filters
+        to tack on the changelist filters to the redirect url.
+        (add_preserved_filters is also used to modify the links of result items)
+        But add_preserved_filters drops multiple values from a SelectMultiple by 
+        calling dict() on a parsed querystring with multiple values.
+        
+        To preserve all the filters, we must readd these dropped values to the query string.
+        """
+        preserved_filters = self.get_preserved_filters(request)
+        preserved_filters = dict(parse_qsl(preserved_filters))
+        response = super()._response_post_save(request, obj)
+        if not self.has_view_or_change_permission(request) or \
+            '_changelist_filters' not in preserved_filters:
+            # Redirects back to the index or no changelist filters were preserved;
+            # no need to act.
+            return response
+        post_url = response.url
+        parsed_url = urlparse(post_url)
+        post_url_query = QueryDict(parsed_url.query, mutable = True)
+        # Update the query string with any lists that preserved_filters contains.
+        for lookup, values_list in QueryDict(preserved_filters['_changelist_filters']).lists():
+            if lookup in post_url_query:
+                post_url_query.setlist(lookup, values_list)
+        # Add the update query string to the url.
+        parsed_url = list(parsed_url)
+        parsed_url[4] = post_url_query.urlencode()
+        post_url = urlunparse(parsed_url)
+        return HttpResponseRedirect(post_url)
         
 class MIZAdminSearchFormMixin(AdminSearchFormMixin):
     
