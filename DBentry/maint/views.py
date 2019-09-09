45b7a6d892d6c8f89a0b4f88dc090b678ce6dc9a
Actionb <yummytea1@gmail.com>
2019-09-09 12:30:57 +0200

    Style conventions

--- DBentry/maint/views.py
+++ DBentry/maint/views.py
@@ -1,17 +1,17 @@
 from django import views
-from django.shortcuts import redirect 
+from django.apps import apps
+from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
+from django.db.models import Count
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
 
-from django.urls import reverse_lazy, reverse 
-from django.db.models import Count 
-from django.apps import apps 
-from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME 
-
+from DBentry import utils
 from DBentry.actions.views import MergeViewWizarded
 from DBentry.base.views import MIZAdminMixin
 from DBentry.sites import register_tool
-from DBentry.utils import get_obj_link, get_model_from_string, ensure_jquery
-
-from DBentry.maint.forms import DuplicateFieldsSelectForm, duplicatefieldsform_factory, ModelSelectForm
+from DBentry.maint.forms import (
+    DuplicateFieldsSelectForm, duplicatefieldsform_factory, ModelSelectForm
+)
 
 # TODO: these maint views are a mess!
 # Make ModelSelectView an 'abstract' view that handles redirection with the right model
@@ -20,7 +20,7 @@ from DBentry.maint.forms import DuplicateFieldsSelectForm, duplicatefieldsform_f
 #@register_tool
 class MaintView(MIZAdminMixin, views.generic.TemplateView):
     template_name = 'admin/basic.html'  # FIXME: this template is now only used for maint views, rename it
-    success_url = reverse_lazy('admin:index') 
+    success_url = reverse_lazy('admin:index')
     title = 'Wartung'
 
     # FIXME: attributes used only in conjunction with admin/basic.html, remove them?
@@ -31,32 +31,35 @@ class MaintView(MIZAdminMixin, views.generic.TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         if self.submit_value: context['submit_value'] = self.submit_value
-        if self.submit_name: context['submit_name']  = self.submit_name
+        if self.submit_name: context['submit_name'] = self.submit_name
         context['form_method'] = self.form_method
         return context
 
-    @staticmethod 
-    def show_on_index_page(request): 
-        # Only show a link on the index page if superuser
-        return request.user.is_superuser 
+    @staticmethod
+    def show_on_index_page(request):
+        # Only show a link on the index page for superusers.
+        return request.user.is_superuser
 
     @classmethod
     def permission_test(cls, request):
-        # Only allow superusers to access the page
-        return request.user.is_superuser 
+        # Only allow superusers to access the page.
+        return request.user.is_superuser
 
-class UnusedObjectsView(MaintView): 
-    model_name = '' 
-    lte = 0 
 
-    def get(self, request, *args, **kwargs): 
-        lte = self.kwargs.get('lte', self.lte) 
-        model_name = self.kwargs.get('model', self.model_name) 
-        url = reverse('admin:DBentry_{}_changelist'.format(model_name)) 
-        model = apps.get_model('DBentry', model_name) 
-        qs = model.objects.annotate(num_a=Count('artikel')).filter(num_a__lte=lte) 
-        request.session['qs'] = dict(id__in=list(qs.values_list('pk', flat=True))) 
-        return redirect(url) 
+class UnusedObjectsView(MaintView):
+
+    model_name = ''
+    lte = 0
+
+    def get(self, request, *args, **kwargs):
+        lte = self.kwargs.get('lte', self.lte)
+        model_name = self.kwargs.get('model', self.model_name)
+        url = reverse('admin:DBentry_{}_changelist'.format(model_name))
+        model = apps.get_model('DBentry', model_name)
+        qs = model.objects.annotate(num_a=Count('artikel')).filter(num_a__lte=lte)
+        request.session['qs'] = dict(id__in=list(qs.values_list('pk', flat=True)))
+        return redirect(url)
+
 
 @register_tool(
     url_name='dupes_select',
@@ -64,17 +67,17 @@ class UnusedObjectsView(MaintView):
     superuser_only=True
 )
 class DuplicateObjectsView(MaintView):
-    #NOTE: check for 'get_duplicates' (name of submit button) in request.GET before doing a query for duplicates? 
+    #NOTE: check for 'get_duplicates' (name of submit button) in request.GET before doing a query for duplicates?
 
     template_name = 'admin/dupes.html'
     form_class = DuplicateFieldsSelectForm
     _dupe_fields = None
 
     def dispatch(self, request, *args, **kwargs):
-        self.model = get_model_from_string(kwargs.get('model_name'))
+        self.model = utils.get_model_from_string(kwargs.get('model_name'))
         self.opts = self.model._meta
-        self.title = 'Duplikate: ' + self.opts.verbose_name 
-        self.breadcrumbs_title = self.opts.verbose_name 
+        self.title = 'Duplikate: ' + self.opts.verbose_name
+        self.breadcrumbs_title = self.opts.verbose_name
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
@@ -104,7 +107,7 @@ class DuplicateObjectsView(MaintView):
             media += context['form'].media
         else:
             media = context['form'].media
-        context['media'] = ensure_jquery(media)
+        context['media'] = utils.ensure_jquery(media)
 
         context['headers'], context['items'] = self.build_duplicate_items_context(context['form'], self.dupe_fields)
         context['action_name'] = 'merge_records'
@@ -115,7 +118,7 @@ class DuplicateObjectsView(MaintView):
     def dupe_fields(self):
         if self._dupe_fields is None:
             self._dupe_fields = []
-            for formfield in self.form_class.base_fields: 
+            for formfield in self.form_class.base_fields:
                 if formfield in self.request.GET:
                     self._dupe_fields.extend(self.request.GET.getlist(formfield))
         return self._dupe_fields
@@ -127,7 +130,7 @@ class DuplicateObjectsView(MaintView):
 
     def build_duplicate_items_context(self, form = None, dupe_fields = None):
         """
-        Returns a list of headers and a list of 2-tuples of:
+        Return a list of headers and a list of 2-tuples of:
             - a list of duplicate items (instance, link to change view, duplicate values)
             - a link to the changelist of these items
         """
@@ -156,13 +159,14 @@ class DuplicateObjectsView(MaintView):
         headers = [choices[f] for f in dupe_fields]
         for instances, values in duplicates:
             dupe_item = [
-                (instance, get_obj_link(instance, self.request.user, include_name = False), [values[f] for f in dupe_fields]) #FIXME: values is a tuple!
+                (instance, utils.get_obj_link(instance, self.request.user, include_name = False), [values[f] for f in dupe_fields]) #FIXME: values is a tuple!
                 for instance in instances
-            ]           
-            cl_url =  reverse('admin:{}_{}_changelist'.format(self.opts.app_label, self.opts.model_name))
+            ]
+            cl_url = reverse('admin:{}_{}_changelist'.format(self.opts.app_label, self.opts.model_name))
             cl_url += '?id__in={}'.format(",".join([str(instance.pk) for instance in instances]))
             items.append((dupe_item, cl_url))
         return headers, items
+
 
 class ModelSelectView(MaintView, views.generic.FormView):
 
@@ -173,7 +177,5 @@ class ModelSelectView(MaintView, views.generic.FormView):
 
     def get(self, request, *args, **kwargs):
         if request.GET.get('model_select'):
-            return redirect(reverse(self.next_view, kwargs = {'model_name':request.GET.get('model_select')}))
+            return redirect(reverse(self.next_view, kwargs = {'model_name': request.GET.get('model_select')}))
         return super().get(request, *args, **kwargs)
-
-
