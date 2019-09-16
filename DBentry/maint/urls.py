f037d78bcc5ac8d7b1bd22f2685784af3dc29907
Actionb <yummytea1@gmail.com>
2019-09-16 09:22:49 +0200

    Update maint.urls

--- DBentry/maint/urls.py
+++ DBentry/maint/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from DBentry.maint.views import DuplicateObjectsView, ModelSelectView
+
+from DBentry.maint.views import DuplicateObjectsView, DuplicateModelSelectView
 
 urlpatterns = [
-    path('dupes/<str:model_name>/', DuplicateObjectsView.as_view(), name = 'dupes'), 
-    #TODO: path('unused/<str:model_name>/lte<int:lte>/', UnusedObjectsView.as_view(), name = 'unused_objects'), 
-    path('dupes/', ModelSelectView.as_view(title='Duplikate', next_view = 'dupes'), name='dupes_select'), 
-    #TODO: path('unused/', ModelSelectView.as_view(title='Unbenutzt', next_view = 'unused_objects'), name='unused_select'), 
-    #TODO: path('', MaintView.as_view(), name='maint_index'),
+    path('dupes/<str:model_name>/', DuplicateObjectsView.as_view(), name = 'dupes'),
+    path('dupes/', DuplicateModelSelectView.as_view(), name='dupes_select'),
 ]
