bfbcd089a2855a7e91b32797751a327c38df4b18
Actionb <yummytea1@gmail.com>
2019-09-20 09:26:00 +0200

    Reworked tag favoriten_link A link is now always displayed no matter whether the model has relations to a favoriten-model.

--- DBentry/templatetags/object_tools.py
+++ DBentry/templatetags/object_tools.py
@@ -2,7 +2,7 @@ from django.urls import reverse, exceptions
 from django.template import Library
 
 from DBentry.help.templatetags import help_link
-from DBentry.utils import get_model_admin_for_model, make_simple_link
+from DBentry.utils import make_simple_link
 
 register = Library()
 
@@ -10,29 +10,16 @@ register = Library()
 @register.simple_tag(takes_context=True)
 def favorites_link(context):
     """Return a link to the 'favorites' view."""
-    if 'opts' not in context:
+    try:
+        url = reverse('favoriten')
+    except exceptions.NoReverseMatch:
         return ''
-
-    model_admin = get_model_admin_for_model(context['opts'].model)
-
-    inline_models = [
-        getattr(inline, 'verbose_model', inline.model)._meta.model_name
-        for inline in model_admin.inlines
-        if getattr(inline, 'verbose_model', inline.model)
-    ]
-
-    if 'genre' in inline_models or 'schlagwort' in inline_models:# model_admin.uses_favorites(): #TODO: implement uses_favorites
-        try:
-            url = reverse('favoriten')
-        except exceptions.NoReverseMatch:
-            return ''
-        return make_simple_link(
-            url =url,
-            label='Favoriten',
-            is_popup=context.get('is_popup', False),
-            as_listitem=True
-        )
-    return ''
+    return make_simple_link(
+        url =url,
+        label='Favoriten',
+        is_popup=context.get('is_popup', False),
+        as_listitem=True
+    )
 
 
 register.simple_tag(takes_context=True)(help_link)
--- DBentry/tests/test_templatetags.py
+++ DBentry/tests/test_templatetags.py
@@ -1,25 +1,19 @@
 import re
 
-from .base import RequestTestCase
-
 from DBentry import models as _models
 from DBentry.templatetags.object_tools import favorites_link
+from DBentry.tests.base import RequestTestCase
+
 
 class TestObjectToolsTags(RequestTestCase):
-        
+
     def test_favorite_links(self):
         # No popup
         expected = '<li><a href="/admin/tools/favoriten/" target="_blank">Favoriten</a></li>'
         links = re.findall('<li>.+?</li>', favorites_link({'opts': _models.artikel._meta, 'is_popup': False}))
         self.assertIn(expected, links)
-        
-        # No favoriten for that model
-        expected = '<li><a href="/admin/tools/favoriten/" target="_blank">Favoriten</a></li>'
-        links = re.findall('<li>.+?</li>', favorites_link({'opts': _models.sender._meta, 'is_popup': False}))
-        self.assertNotIn(expected, links)
-        
+
         # As popup
         expected = '<li><a href="/admin/tools/favoriten/?_popup=1" onclick="return popup(this)">Favoriten</a></li>'
         links = re.findall('<li>.+?</li>', favorites_link({'opts': _models.artikel._meta, 'is_popup': True}))
         self.assertIn(expected, links)
-        
