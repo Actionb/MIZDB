9d46a0c1cef3f7504244dc75a40cc99dc94a8694
Actionb <yummytea1@gmail.com>
2019-09-20 08:55:19 +0200

    Shortenend empty lines

--- DBentry/templatetags/object_tools.py
+++ DBentry/templatetags/object_tools.py
@@ -5,20 +5,20 @@ from django.template import Library
 from DBentry.utils import get_model_admin_for_model, make_simple_link
 
 register = Library()
-    
+
 @register.simple_tag(takes_context = True)
 def favorites_link(context):
     if 'opts' not in context:
         return ''
-        
+
     model_admin = get_model_admin_for_model(context['opts'].model)
-        
+
     inline_models = [
         getattr(inline, 'verbose_model', inline.model)._meta.model_name
         for inline in model_admin.inlines
         if getattr(inline, 'verbose_model', inline.model)
     ]
-    
+
     if 'genre' in inline_models or 'schlagwort' in inline_models:# model_admin.uses_favorites(): #TODO: implement uses_favorites
         try:
             url = reverse('favoriten')
@@ -26,14 +26,14 @@ def favorites_link(context):
             return ''
         return make_simple_link(url = url, label = 'Favoriten', is_popup = context.get('is_popup', False), as_listitem = True)
     return ''
-        
+
 from DBentry.help.templatetags import help_link
 register.simple_tag(takes_context = True)(help_link)
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
