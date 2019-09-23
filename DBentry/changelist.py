685504108a440739c3998f3a50ad6463f7872c3b
Actionb <yummytea1@gmail.com>
2019-09-23 14:06:37 +0200

    Code style: changelist

--- DBentry/changelist.py
+++ DBentry/changelist.py
@@ -1,16 +1,18 @@
+from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
 from django.contrib import admin
 from django.contrib.admin.views.main import ChangeList
 from django.db import models
-from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
 from django.utils.datastructures import MultiValueDict
 
 from DBentry.search.admin import ChangelistSearchFormMixin
 
+
 class MIZChangeList(ChangelistSearchFormMixin, ChangeList):
 
     def get_queryset(self, request):
-        """ Copy pasted from original ChangeList to switch around ordering and filtering.
-            Also allowed the usage of Q items to filter the queryset.
+        """
+        Copy pasted from django's ChangeList to switch around ordering and
+        filtering. Also allow the usage of Q items to filter the queryset.
         """
         # First, we collect all the declared list filters.
         (self.filter_specs, self.has_filters, remaining_lookup_params,
@@ -32,16 +34,16 @@ class MIZChangeList(ChangelistSearchFormMixin, ChangeList):
                         if isinstance(value, models.Q):
                             qs = qs.filter(value)
                         else:
-                            qs = qs.filter(**{key:value})
+                            qs = qs.filter(**{key: value})
             else:
                 # NOTE: isn't remaining_lookup_params always a MultiValueDict?
                 for k, v in remaining_lookup_params.items():
                     if isinstance(v, models.Q):
                         qs = qs.filter(v)
                     else:
-                        qs = qs.filter(**{k:v})
+                        qs = qs.filter(**{k: v})
 
-        except (SuspiciousOperation, ImproperlyConfigured) as e:
+        except (SuspiciousOperation, ImproperlyConfigured):
             # Allow certain types of errors to be re-raised as-is so that the
             # caller can treat them in a special way.
             raise
@@ -51,14 +53,16 @@ class MIZChangeList(ChangelistSearchFormMixin, ChangeList):
             # invalid if the keyword arguments are incorrect, or if the values
             # are not in the correct type, so we might get FieldError,
             # ValueError, ValidationError, or ?.
-            #TODO: a advsf formfield may throw a ValidationError!
+            # TODO: a advsf formfield may throw a ValidationError!
             raise admin.options.IncorrectLookupParameters(e)
 
         if not qs.query.select_related:
             qs = self.apply_select_related(qs)
 
         # Apply search results
-        qs, search_use_distinct = self.model_admin.get_search_results(request, qs, self.query)
+        qs, search_use_distinct = self.model_admin.get_search_results(
+            request, qs, self.query
+        )
 
         # Get ordering, record and apply annotations and then set the ordering.
         ordering = self.get_ordering(request, qs)
@@ -73,8 +77,8 @@ class MIZChangeList(ChangelistSearchFormMixin, ChangeList):
     def apply_ordering(self, request, queryset, ordering):
         return queryset.order_by(*ordering)
 
+
 class AusgabeChangeList(MIZChangeList):
 
     def apply_ordering(self, request, queryset, ordering):
         return queryset.chronologic_order(ordering)
-
