a4d68aeb1968cff35fa6163ecfe9bab032583712
Actionb <yummytea1@gmail.com>
2019-09-26 13:20:18 +0200

    cleaned up DBentry.views

--- DBentry/views.py
+++ DBentry/views.py
@@ -20,31 +20,34 @@ class FavoritenView(MIZAdminMixin, PermissionRequiredMixin, views.generic.Update
     ]
 
     def get_success_url(self):
-        # Redirect back onto this site
+        # Redirect back to this site.
         return ''
 
     def get_object(self):
-        # user field on Favoriten is unique, so at most a single user can have one set of favorites or none
+        # user field on Favoriten is unique, so at most a single user can have
+        # one set of favorites or none.
         object = self.model.objects.filter(user=self.request.user).first()
         if object is None:
             # user has no favorites yet, create an entry in Favoriten model
             object = self.model(user=self.request.user)
             object.save()
-        return object        
+        return object
 
 
 # views for the django default handlers
 def MIZ_permission_denied_view(request, exception, template_name='admin/403.html'):
     from django.template import TemplateDoesNotExist, loader
     from django import http
+    # Make sure that a template for template_name exists.
     try:
         template = loader.get_template(template_name)
     except TemplateDoesNotExist:
-        return http.HttpResponseForbidden('<h1>403 Forbidden</h1>', content_type='text/html')
+        return http.HttpResponseForbidden(
+            '<h1>403 Forbidden</h1>', content_type='text/html')
 
     from django.template.response import TemplateResponse
     msg = 'Sie haben nicht die erforderliche Berechtigung diese Seite zu sehen.'
-    context = {'exception' : str(exception) if str(exception) else msg}
+    context = {'exception': str(exception) if str(exception) else msg}
     context.update(miz_site.each_context(request))
-    context['is_popup'] = '_popup' in request.GET 
+    context['is_popup'] = '_popup' in request.GET
     return TemplateResponse(request, template_name, context=context)
