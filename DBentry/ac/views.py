41a38a4b2608a19912d4f5700d463b31045166e6
Actionb <yummytea1@gmail.com>
2019-10-01 11:04:45 +0200

    ac.views: Code style, docstrings and TODOs

--- DBentry/ac/views.py
+++ DBentry/ac/views.py
@@ -1,19 +1,21 @@
 from django import http
+from django.contrib.auth import get_permission_codename
 from django.db.models import Q
 from django.utils.translation import gettext
 
 from dal import autocomplete
 
+from DBentry.ac.creator import Creator
 from DBentry.models import Favoriten, ausgabe, buch
 from DBentry.logging import LoggingMixin
 from DBentry.utils import get_model_from_string
-from DBentry.ac.creator import Creator
 
 
 class ACBase(autocomplete.Select2QuerySetView, LoggingMixin):
+    """Base view for the autocomplete views of the DBentry app."""
 
     _search_fields = None
-    create_field = None
+    create_field = None  # NOTE: this already is an attribute of dal.BaseQuerySetView
 
     def dispatch(self, *args, **kwargs):
         if not self.model:
@@ -59,25 +61,32 @@ class ACBase(autocomplete.Select2QuerySetView, LoggingMixin):
         # 'model' has been set with certainty:
         # either the model attribute is set by the subclass or it is set with
         # the captured model name during dispatch
+        # FIXME: this is not used by ANYTHING?
         if not self._search_fields:
             self._search_fields = self.model.get_search_fields()
         return self._search_fields
 
     def do_ordering(self, qs):
+        # FIXME: reapplying the model's default ordering could remove ordering
+        # set by a declared MultipleObjectMixin.queryset attribute.
         return qs.order_by(*self.model._meta.ordering)
 
     def apply_q(self, qs, use_suffix=True, ordered=True):
+        """Filter the given queryset 'qs' with the view's search term 'q'."""
+        # FIXME: 'ordered' argument is always True?
+        # If only AusgabeQuerySet uses find(ordered=True), why not override
+        # ACAusgabe.apply_q?
         if self.q:
             return qs.find(self.q, ordered=ordered, use_suffix=use_suffix)
         elif self.model in Favoriten.get_favorite_models():
-            # add Favoriten to the top of the result queryset
+            # Add Favoriten to the top of the result queryset
             # if no search term was given.
             try:
                 favorites = Favoriten.objects.get(user=self.request.user)
             except Favoriten.DoesNotExist:
                 return qs
-            # if there are no favorites for the model, an empty queryset will
-            # be returned by get_favorites
+            # If there are no favorites for the model an empty queryset will
+            # be returned by get_favorites.
             return list(favorites.get_favorites(self.model)) + list(qs)
         else:
             return qs
@@ -90,7 +99,8 @@ class ACBase(autocomplete.Select2QuerySetView, LoggingMixin):
         return obj
 
     def get_queryset(self):
-        # TODO: MultipleObjectMixin.get_queryset ?
+        """Return the ordered and filtered queryset for this view."""
+        # TODO: rely on MultipleObjectMixin.get_queryset ?
         if self.queryset is None:
             qs = self.model.objects.all()
         else:
@@ -104,7 +114,7 @@ class ACBase(autocomplete.Select2QuerySetView, LoggingMixin):
                         qobjects |= Q((k, v))
                 qs = qs.filter(qobjects)
             else:
-                # All forwarded values were None, return an empty queryset
+                # All forwarded values were None, return an empty queryset.
                 return self.model.objects.none()
 
         qs = self.do_ordering(qs)
@@ -112,16 +122,12 @@ class ACBase(autocomplete.Select2QuerySetView, LoggingMixin):
         return qs
 
     def has_add_permission(self, request):
-        # Overwritten since get_queryset() may return a list
-        # (of exact matches, startswith matches and contains matches) now too.
-        # Plus, autocomplete views have a model attribute anyhow. This avoids
-        # doing anything expensive in get_queryset.
         """Return True if the user has the permission to add a model."""
         if not request.user.is_authenticated:
             return False
-
-        # opts = self.get_queryset().model._meta <--- Overwritten
-        from django.contrib.auth import get_permission_codename
+        # At this point, dal calls get_queryset() to get the model options via
+        # queryset.model._meta which is unnecessary for ACBase since it
+        # declares the model class during dispatch().
         opts = self.model._meta
         codename = get_permission_codename('add', opts)
         return request.user.has_perm("%s.%s" % (opts.app_label, codename))
@@ -144,11 +150,20 @@ class ACBase(autocomplete.Select2QuerySetView, LoggingMixin):
 
 
 class ACBuchband(ACBase):
+    """
+    Autocomplete view that queries buch instances that are defined as 'buchband'.
+    """
+
     model = buch
     queryset = buch.objects.filter(is_buchband=True)
 
 
 class ACAusgabe(ACBase):
+    """
+    Autocomplete view for the model ausgabe that applies chronologic order to
+    the results.
+    """
+
     model = ausgabe
 
     def do_ordering(self, qs):
@@ -156,11 +171,18 @@ class ACAusgabe(ACBase):
 
 
 class ACCreateable(ACBase):
+    """
+    Add additional information to the create_option part of the response and
+    enable a more involved model instance creation process by utilizing a
+    Creator helper object.
+    """
 
     def dispatch(self, *args, **kwargs):
         if not self.model:
             model_name = kwargs.pop('model_name', '')
             self.model = get_model_from_string(model_name)
+        # TODO: use a get_creator() method to avoid having to declare
+        # self.creator in dispatch()
         self.creator = Creator(self.model, raise_exceptions=False)
         # TODO: super().dispatch() also sets create_field
         self.create_field = self.create_field or kwargs.pop('create_field', None)
@@ -168,12 +190,12 @@ class ACCreateable(ACBase):
 
     def createable(self, text, creator=None):
         """
-        Returns True if a new(!) model instance can be created from `text`.
+        Return True if a new(!) model instance would be created from 'text'.
         """
         creator = creator or self.creator
         created = creator.create(text, preview=True)
         pk = getattr(created.get('instance', None), 'pk', None)
-        if (created and pk is None):
+        if created and pk is None:
             return True
         return False
 
@@ -184,12 +206,18 @@ class ACCreateable(ACBase):
         if q:
             page_obj = context.get('page_obj', None)
             if page_obj is None or not self.has_more(context):
-                # See if we can create an object from q
+                # See if we can create a new object from q.
+                # If pre-existing objects can be found using q, the create
+                # option should not be enabled.
                 if self.createable(q):
                     return True
         return False
 
     def build_create_option(self, q):
+        """
+        Add additional information on how the object is going to be created
+        to the create option.
+        """
         create_option = super().build_create_option(q)
         create_info = self.get_creation_info(q)
         if create_info:
@@ -214,18 +242,19 @@ class ACCreateable(ACBase):
         creator = creator or self.creator
         create_info = []
         default = {
-            'id': None,  # 'id' : None will make the option unselectable
+            'id': None,  # 'id': None will make the option unselectable.
             'create_id': True, 'text': '...mit folgenden Daten:'
         }
 
         create_info.append(default.copy())
-        # iterate over all nested dicts in create_info returned by creator.create
+        # Iterate over all nested dicts in create_info returned by the creator.
         for k, v in flatten_dict(creator.create(text, preview=True)):
             default['text'] = str(k) + ': ' + str(v)
             create_info.append(default.copy())
         return create_info
 
     def create_object(self, text, creator=None):
+        """Create a model instance from 'text' and save it to the database."""
         if self.has_create_field():
             return super().create_object(text)
         creator = creator or self.creator
