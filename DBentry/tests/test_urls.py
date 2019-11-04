38bd5704e70500d0a72f476e133cb3632b7e26f5
Actionb <yummytea1@gmail.com>
2019-11-04 11:03:58 +0100

    more code style stuff

--- DBentry/tests/test_urls.py
+++ DBentry/tests/test_urls.py
@@ -1,23 +1,22 @@
 from django.urls import reverse, resolve
 from django.urls.exceptions import NoReverseMatch, Resolver404
 
-from .base import MyTestCase
-
-from DBentry.sites import miz_site
-from DBentry.bulk import views as bulk_views
-
-from MIZDB import urls as mizdb_urls
 from DBentry import urls as dbentry_urls, views as dbentry_views
 from DBentry.ac import urls as autocomplete_urls, views as autocomplete_views
-#from DBentry.help import urls as help_urls, views as help_views
+from DBentry.bulk import views as bulk_views
 from DBentry.maint import urls as maint_urls, views as maint_views
+from DBentry.sites import miz_site
+from DBentry.tests.base import MyTestCase
+
+from MIZDB import urls as mizdb_urls
+
 
 class URLTestCase(MyTestCase):
-    
+
     urlconf = None
     current_app = None
-    
-    def assertReverses(self, view_name, expected  = None, *args, **kwargs):
+
+    def assertReverses(self, view_name, expected=None, *args, **kwargs):
         if 'urlconf' in kwargs:
             urlconf = kwargs.pop('urlconf')
         else:
@@ -26,22 +25,23 @@ class URLTestCase(MyTestCase):
             current_app = kwargs.pop('current_app')
         else:
             current_app = self.current_app
-        
+
         try:
-            reversed = reverse(view_name, args = args, kwargs = kwargs, urlconf = urlconf, current_app = current_app)
+            reversed = reverse(
+                view_name, args=args, kwargs=kwargs, urlconf=urlconf, current_app=current_app)
         except NoReverseMatch as e:
             raise AssertionError(e.args[0])
         if expected is not None:
             self.assertEqual(reversed, expected)
-        
-    def assertResolves(self, url, expected = None, **kwargs):
+
+    def assertResolves(self, url, expected=None, **kwargs):
         if 'urlconf' in kwargs:
             urlconf = kwargs.pop('urlconf')
         else:
             urlconf = self.urlconf
-            
+
         try:
-            resolved = resolve(url, urlconf = urlconf)
+            resolved = resolve(url, urlconf=urlconf)
         except Resolver404 as e:
             raise AssertionError(e.args[0])
         if expected is not None:
@@ -49,50 +49,53 @@ class URLTestCase(MyTestCase):
                 self.assertEqual(resolved.func.view_class, expected)
             else:
                 self.assertEqual(resolved.func.__wrapped__.__func__, expected)
-            
+
+
 class TestURLs(URLTestCase):
-    
+
     def test_mizdb_urls(self):
-        # Tests the root urls in MIZDB.urls.py
+        # Tests the root urls in MIZDB.urls.py.
         self.urlconf = mizdb_urls
         self.assertReverses('admin:index', '/admin/')
-        self.assertResolves('/admin/', miz_site.index.__func__) # miz_site.index is a bound function
-        
-        self.assertReverses('admin:app_list', app_label = 'DBentry')
+        self.assertResolves('/admin/', miz_site.index.__func__)  # miz_site.index is a bound function
+
+        self.assertReverses('admin:app_list', app_label='DBentry')
         self.assertResolves('/admin/DBentry/', miz_site.app_index.__func__)
-        
+
     def test_dbentry_urls(self):
-        # Tests the urls in DBentry.urls.py
+        # Tests the urls in DBentry.urls.py.
         self.urlconf = dbentry_urls
-        
+
         expected = [
-            ('bulk_ausgabe', '/tools/bulk_ausgabe/', bulk_views.BulkAusgabe), 
+            ('bulk_ausgabe', '/tools/bulk_ausgabe/', bulk_views.BulkAusgabe),
             ('favoriten', '/tools/favoriten/', dbentry_views.FavoritenView)
         ]
         for view_name, url, view_class in expected:
             with self.subTest(view_name=view_name, url=url):
                 self.assertReverses(view_name, url)
                 self.assertResolves(url, view_class)
-        
+
     def test_autocomplete_urls(self):
-        # Tests the urls in DBentry.ac.urls.py
+        # Tests the urls in DBentry.ac.urls.py.
         self.urlconf = autocomplete_urls
-        
         expected = [
-            ('acbuchband', '/buch/', (), {}, autocomplete_views.ACBuchband), 
-            ('acausgabe', '/ausgabe/', (), {}, autocomplete_views.ACAusgabe), 
-            ('accapture', '/musiker/kuenstler_name/', (), 
-                {'model_name': 'musiker', 'create_field': 'kuenstler_name'}, autocomplete_views.ACBase), 
+            ('acbuchband', '/buch/', (), {}, autocomplete_views.ACBuchband),
+            ('acausgabe', '/ausgabe/', (), {}, autocomplete_views.ACAusgabe),
+            (
+                'accapture', '/musiker/kuenstler_name/', (),
+                {'model_name': 'musiker', 'create_field': 'kuenstler_name'},
+                autocomplete_views.ACBase
+            ),
             ('accapture', '/autor/', (), {'model_name': 'autor'}, autocomplete_views.ACCreateable)
         ]
         for view_name, url, args, kwargs, view_class in expected:
             with self.subTest(view_name=view_name, url=url):
                 self.assertReverses(view_name, url, *args, **kwargs)
                 self.assertResolves(url, view_class)
-        
+
     def test_help_urls(self):
         pass
-        
+
     def test_maint_urls(self):
         self.urlconf = maint_urls
         test_data = [
@@ -101,7 +104,7 @@ class TestURLs(URLTestCase):
                 maint_views.DuplicateModelSelectView
             ),
             (
-                'dupes', '/dupes/ausgabe/', (), {'model_name':'ausgabe'},
+                'dupes', '/dupes/ausgabe/', (), {'model_name': 'ausgabe'},
                 maint_views.DuplicateObjectsView
             )
         ]
