17034d7a8165b19e80a67dbf0406002545b2ff25
Actionb <yummytea1@gmail.com>
2020-07-09 14:41:37 +0200

    Split get_changelist_link into two: get_changelist_url provides the url to the changelist get_changelist_link provides an html element to that changelist

--- DBentry/utils/admin.py
+++ DBentry/utils/admin.py
@@ -47,9 +47,9 @@ def get_obj_link(obj, user, site_name='admin', include_name=True):
     return format_html('<a href="{}">{}</a>', admin_url, obj)
 
 
-def get_changelist_link(model, user, site_name='admin', obj_list=None):
+def get_changelist_url(model, user, site_name='admin', obj_list=None):
     """
-    Return a safe link to the changelist of 'model'.
+    Return an url to the changelist of 'model'.
 
     If 'obj_list' is given, the url to the changelist will include a query
     param to filter to records in that list.
@@ -69,7 +69,22 @@ def get_changelist_link(model, user, site_name='admin', obj_list=None):
         return ''
     if obj_list:
         url += '?id={}'.format(",".join([str(obj.pk) for obj in obj_list]))
-    return format_html('<a href="{}">Liste</a>', url)
+    return url
+
+
+def get_changelist_link(model, user, site_name='admin', obj_list=None):
+    """
+    Return a safe link to the changelist of 'model'.
+
+    If 'obj_list' is given, the url to the changelist will include a query
+    param to filter to records in that list.
+    """
+    return format_html(
+        '<a href="{}">Liste</a>',
+        get_changelist_url(
+            model, user, site_name='admin', obj_list=obj_list
+        )
+    )
 
 
 def link_list(request, obj_list, sep=", "):
