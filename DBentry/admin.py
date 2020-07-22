5f2ac81cacab3542546e50630276dc30536a3718
Actionb <yummytea1@gmail.com>
2020-07-22 11:08:14 +0200

    Added 'signatur' search form field to BildmaterialAdmin

--- DBentry/admin.py
+++ DBentry/admin.py
@@ -380,8 +380,8 @@ class BildmaterialAdmin(MIZModelAdmin):
         PersonInLine, BestandInLine
     ]
     search_form_kwargs = {
-        'fields': [  # TODO: add signatur field to search form
-            'datum__range', 'schlagwort', 'genre', 'band','musiker', 'reihe', 'id__in'
+        'fields': [
+            'datum__range', 'schlagwort', 'genre', 'band','musiker', 'reihe', 'signatur', 'id__in'
         ],
         'labels': {'reihe': 'Bildreihe'}
     }
