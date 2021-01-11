9b636445ea51ac93da1a03710fad431c20a405cc
Actionb <yummytea1@gmail.com>
2021-01-11 12:48:32 +0100

    Renamed folder

--- DBentry/static/admin/js/googlebtn.js
+++
@@ -1,7 +0,0 @@
-googleMe = function(field_name){
-	var ele_name = "id_".concat(field_name);
-    var val = encodeURIComponent(document.getElementById(ele_name).value.trim());
-   	if(val){
-    	window.open("https://www.google.de/search?q=".concat(val),"_blank");
-    }
-};
