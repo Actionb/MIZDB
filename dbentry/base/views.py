065baf47e27a6a05843345645eb3d9fa8c512bae
Actionb <yummytea1@gmail.com>
2021-09-03 13:56:24 +0200

    dbentry.base: code style, type hints, docstrings

--- dbentry/base/views.py
+++ dbentry/base/views.py
@@ -1,6 +1,10 @@
-from django import views
-from django.contrib.auth.mixins import UserPassesTestMixin
+from typing import Any, Optional, Type
 
+from django import views
+from django.contrib.admin import AdminSite
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.forms import Form
+from django.http import HttpRequest, HttpResponse
 from formtools.wizard.views import SessionWizardView
 
 from dbentry.sites import miz_site
@@ -9,17 +13,18 @@ from dbentry.sites import miz_site
 class MIZAdminMixin(object):
     """Add admin_site specific context (each_context) to the view."""
 
-    title = None
-    site_title = 'MIZDB'
-    breadcrumbs_title = None
-    admin_site = miz_site
+    title: str = ''
+    site_title: str = 'MIZDB'
+    breadcrumbs_title: str = ''
+    admin_site: AdminSite = miz_site
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    # noinspection PyUnresolvedReferences
+    def get_context_data(self, **kwargs: Any) -> dict:
+        context = super().get_context_data(**kwargs)  # type: ignore[misc]
         # Add admin site context.
-        context.update(self.admin_site.each_context(self.request))
+        context.update(self.admin_site.each_context(self.request))  # type: ignore[attr-defined]
         # Enable popups behaviour for custom views.
-        context['is_popup'] = '_popup' in self.request.GET
+        context['is_popup'] = '_popup' in self.request.GET  # type: ignore[attr-defined]
         # Context variables title & site_title for the html document's title.
         # (used by admin/base_site.html)
         if self.title:
@@ -34,13 +39,17 @@ class MIZAdminMixin(object):
 class OptionalFormView(views.generic.FormView):
     """A FormView that does not require form_class to be set."""
 
-    def get_form(self, form_class=None):
+    def get_form(self, form_class: Optional[Type[Form]] = None) -> Optional[Form]:
+        """
+        Return an instance of the form to be used in this view, or None if no
+        form class given.
+        """
         if self.get_form_class() is None:
             # The form is optional.
             return None
         return super().get_form(form_class)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         form = self.get_form()
         if form is None or form.is_valid():
             # Treat an optional form like a valid form.
@@ -53,11 +62,16 @@ class OptionalFormView(views.generic.FormView):
 class FixedSessionWizardView(SessionWizardView):
     """Subclass of SessionWizardView that fixes some quirks."""
 
-    def get_context_data(self, form=None, **kwargs):
+    def get_context_data(self, form: Optional[Form] = None, **kwargs: Any) -> dict:
         # SessionWizardView expects 'form' as required positional argument.
         return super().get_context_data(form, **kwargs)
 
-    def get_form(self, step=None, data=None, files=None):
+    def get_form(
+            self,
+            step: Optional[int] = None,
+            data: Optional[dict] = None,
+            files: Optional[dict] = None
+    ) -> Form:
         """
         WizardView.get_form overrides any alterations to the form kwargs
         made in self.get_form_kwargs:
@@ -70,6 +84,10 @@ class FixedSessionWizardView(SessionWizardView):
                 'initial': self.get_form_initial(step),
             })
             Where 'step', 'data' and 'files' are get_form's arguments.
+
+        If we wanted to keep changes to the request payload made in
+        get_form_kwargs, we need to to call super with explicit data (and files)
+        kwargs derived from get_form_kwargs.
         """
         if step is None:
             step = self.steps.current
@@ -82,6 +100,7 @@ class FixedSessionWizardView(SessionWizardView):
 class SuperUserOnlyMixin(UserPassesTestMixin):
     """Only allow superusers to access the view."""
 
-    def test_func(self):
+    def test_func(self) -> bool:
         """test_func for UserPassesTestMixin."""
+        # noinspection PyUnresolvedReferences
         return self.request.user.is_superuser
