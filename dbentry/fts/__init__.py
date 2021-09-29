1432e1634955b41e20733bf94d82113fa7d724b8
Actionb <yummytea1@gmail.com>
2021-09-29 11:35:08 +0200

    Added package docstring

--- dbentry/fts/__init__.py
+++ dbentry/fts/__init__.py
@@ -1,1 +1,27 @@
+"""
+This package adds a SearchVectorField with WeightedColumns and a QuerySet mixin
+for django's postgres full text search. This package builds upon the python
+package ``django-tsvector-field``, and extends it by allowing setting the
+'language' (i.e. postgres search dictionary/config) of each column of a
+SearchVectorField.
+"""
+"""
+In detail:
+django-tsvector-field's SearchVectorField implementation allows for 'language'
+and 'language_column' (the name of a column in the table that specifies the 
+language) arguments that set the language for the entire field.
+That language is then used on every column - meaning that every column of the
+field will be parsed (``to_tsvector``) using the same language/config.
 
+But we need to able to have columns with different languages contributing to
+the same search vector:
+assuming that a table is about persons with fields for name and description, 
+then the name of a person must not be stemmed (i.e. 'simple' must be used as 
+config), while the description of that person should be parsed according to the
+appropriate natural language.
+
+This package adds that 'per column language'. To that end, changes had to be 
+made to the database triggers added by django-tsvector-field: 
+the vector field arguments 'language' and 'language_column' are ignored and 
+only the language argument of the WeightedColumn declarations matters.
+"""
