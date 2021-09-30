c48e6834a44bd16a18a752b2e694e269381bf660
Actionb <yummytea1@gmail.com>
2021-09-30 12:35:28 +0200

    Tweaked deconstruct methods of WeightedColumn and SearchVectorField

--- dbentry/fts/fields.py
+++ dbentry/fts/fields.py
@@ -17,10 +17,12 @@ class WeightedColumn(tsvector_field.WeightedColumn):
         super().__init__(name, weight)
 
     def deconstruct(self):
-        """Return a tuple with which the column can be recreated."""
-        path = "dbentry.fts.fields.{}".format(self.__class__.__name__)
+        """
+        Return a 3-tuple (path, args, kwargs) with which the column can be
+        recreated.
+        """
         return (
-            path,
+            "dbentry.fts.fields.{}".format(self.__class__.__name__),
             [force_text(self.name), force_text(self.weight), force_text(self.language)],
             {}
         )
@@ -29,8 +31,10 @@ class WeightedColumn(tsvector_field.WeightedColumn):
 class SearchVectorField(tsvector_field.SearchVectorField):
     # Adjust SearchVectorField to skip the _check_language_attributes check.
     
-    def __init__(self, blank=True, null=True, editable=False, *args, **kwargs):
-        super().__init__(blank=blank, null=null, editable=editable, *args, **kwargs)
+    def __init__(self, blank=True, editable=False, *args, **kwargs):
+        # Set defaults for blank and editable. Note that tsvector_field ALWAYS
+        # sets null to True.
+        super().__init__(blank=blank, editable=editable, *args, **kwargs)
 
     def _check_language_attributes(self, textual_columns):
         # Skip this check.
@@ -38,10 +42,29 @@ class SearchVectorField(tsvector_field.SearchVectorField):
         return []
 
     def deconstruct(self):
-        """Return a tuple with which the field can be recreated."""
+        """
+        Return a 4-tuple (name, path, args, kwargs) with which the field can be
+        recreated.
+        """
+        name, path, args, kwargs = super().deconstruct()
+        # The defaults for blank and editable are the exact opposite of those
+        # of fields.Field. When Field.deconstruct is called, it will try to
+        # omit parameters that have their default value.
+        # Obviously, Field.deconstruct checks the parameter values against ITS
+        # default values - not against the defaults of SearchVectorField.
+        # And since our defaults are the opposite of fields.Field's, that means
+        # that Field.deconstruct will omit those parameters when we need
+        # to include them and it will include them when we should omit them.
+        if self.blank is not True:
+            kwargs['blank'] = False
+        else:
+            kwargs.pop('blank', None)
+        if self.editable is not False:
+            kwargs['editable'] = True
+        else:
+            kwargs.pop('editable', None)
         # Change the path to so that this SearchVectorField class is used
         # instead of the default implementation:
-        name, path, args, kwargs = super().deconstruct()
         return (
             name,
             "dbentry.fts.fields.{}".format(self.__class__.__name__),
