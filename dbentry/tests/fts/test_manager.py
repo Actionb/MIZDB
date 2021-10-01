f06d73bcd9f6be9226820d31f6d2dd000364bc7b
Actionb <yummytea1@gmail.com>
2021-10-01 12:55:02 +0200

    Moved search() tests to tests.fts.test_manager

--- dbentry/tests/fts/test_manager.py
+++ dbentry/tests/fts/test_manager.py
@@ -5,8 +5,117 @@ from django.db import models
 from django.db.models.expressions import CombinedExpression
 from django.test import TestCase
 
+from dbentry import models as _models
+from dbentry.factory import make
 from dbentry.fts.fields import SearchVectorField
 from dbentry.fts.manager import TextSearchQuerySetMixin
+from dbentry.tests.base import DataTestCase
+
+
+class TestFullTextSearch(DataTestCase):
+
+    model = _models.Band
+    raw_data = [
+        {
+            'band_name': 'Die Ärzte',
+            'beschreibung': 'Die deutschen Ärzte mögen Crêpe.',
+            'bandalias__alias': 'El Doktores'
+        },
+        {   # 'control' object:
+            'band_name': 'The Beatles',
+            'beschreibung': 'The Beatles sollten in keinen Suchergebnissen auftauchen.'
+        },
+        {'band_name': 'Übermensch'}
+    ]
+
+    def test_search_simple(self):
+        # Assert that lookups for names return expected results.
+        results = self.queryset.search('Die Ärzte')
+        self.assertEqual(results.count(), 1)
+        self.assertEqual(results.get(), self.obj1)
+
+    def test_search_simple_partial(self):
+        # Assert that partial lookups for names return expected results.
+        for partial_search_term in ['Di', 'Ärz', 'Die Ärz']:
+            with self.subTest(search_term=partial_search_term):
+                results = self.queryset.search(partial_search_term)
+                self.assertEqual(results.count(), 1)
+                self.assertEqual(results.get(), self.obj1)
+
+    def test_search_stemming(self):
+        # Assert that lookups using natural language return expected results.
+        results = self.queryset.search('deutscher Arzt')
+        self.assertEqual(results.count(), 1)
+        self.assertEqual(results.get(), self.obj1)
+
+    def test_unaccent(self):
+        # Assert that accents do not influence the search results.
+        for search_term in ['Crêpe', 'Crépe', 'Crepe']:
+            with self.subTest(search_term=search_term):
+                results = self.queryset.search(search_term)
+                self.assertEqual(results.count(), 1)
+                self.assertEqual(results.get(), self.obj1)
+
+    def test_ranking(self):
+        # Assert that the results are ranked as expected.
+        #
+        # A simple (i.e. not stemmed) query on the field with the heighest
+        # weight (field 'band_name') would match obj1. obj1 should therefore
+        # have the highest ranking.
+        # A stemmed query on 'band_name' would match this next object. Since
+        # the search term contains a stop word, the match isn't as exact as the
+        # simple query match for obj1.
+        other = make(self.model, band_name='Der Arzt')
+        # This object contains part of the stemmed query in a field with lower
+        # weight - it should come last:
+        another = make(
+            self.model, band_name='Toten Hosen',
+            beschreibung='Sie gehen gerne zum Arzt.'
+        )
+        self.assertEqual(
+            list(self.queryset.search('Die Ärzte').order_by('-rank')),
+            [self.obj1, other, another]
+        )
+
+    def test_alias_search(self):
+        # Assert that objects can be found by searching for an alias.
+        results = self.queryset.search('Doktores')
+        self.assertEqual(results.count(), 1)
+        self.assertEqual(results.get(), self.obj1)
+
+    def test_ordering(self):
+        # Assert that the result querysets includes the model's default ordering.
+        results = self.queryset.search('Die Ärzte')
+        for ordering in self.model._meta.ordering:
+            self.assertIn(
+                ordering, results.query.order_by,
+                msg=(
+                    "Result queryset ordering does not include the model's "
+                    "default ordering.\nResult ordering:%s\nDefault ordering:%s" % (
+                        results.query.order_by, self.model._meta.ordering
+                    )
+                )
+            )
+
+    def test_handles_special_characters(self):
+        # Assert that queries with tsquery-specific chars are fine.
+        chars = ['"', "'", "&", "|",  "<", ">", ":"]
+        for special_char in chars:
+            with self.subTest(special_char=special_char):
+                with self.assertNotRaises(Exception):
+                    self.queryset.search('Beep' + special_char)
+
+    def test_search_umlaute(self):
+        # Regression test:
+        # SQLite performs case sensitive searches for strings containing chars
+        # outside the ASCII range (such as Umlaute ä, ö, ü).
+        for q in ('ü', 'Ü'):
+            with self.subTest(q=q):
+                results = self.queryset.search(q)
+                self.assertTrue(
+                    results,
+                    msg="Expected to find matches regardless of case of Umlaut."
+                )
 
 
 class TestTextSearchQuerySetMixin(TestCase):
