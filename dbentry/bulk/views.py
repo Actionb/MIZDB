896a1b8b64d83110c7cf561fb4bc3b631e9cdc52
Actionb <yummytea1@gmail.com>
2022-01-12 13:40:46 +0100

    Removed help page for BulkAusgabe (use the Wiki instead)

--- dbentry/bulk/views.py
+++ dbentry/bulk/views.py
@@ -445,13 +445,3 @@ class BulkAusgabe(MIZAdminMixin, PermissionRequiredMixin, views.generic.FormView
         # Add ausgabe's meta for the template.
         # noinspection PyProtectedMember,PyUnresolvedReferences
         return super().get_context_data(opts=_models.Ausgabe._meta)
-
-
-# TODO: remove BulkAusgabeHelp -- wiki has this covered
-class BulkAusgabeHelp(MIZAdminMixin, views.generic.TemplateView):
-    """
-    A very basic view containing some text explaining the BulkAusgabe view.
-    """
-
-    template_name = 'admin/help_bulk_ausgabe.html'
-    title = "Hilfe: Ausgaben Erstellung"
