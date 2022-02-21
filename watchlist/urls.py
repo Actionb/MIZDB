bf125d3f43c3b81bb492f7b965cb79bf34326cc8
Actionb <yummytea1@gmail.com>
2022-02-21 12:58:17 +0100

    Removed default watchlist view URL

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from watchlist.views import WatchlistView, watchlist_changelist, watchlist_toggle
+from watchlist.views import watchlist_changelist, watchlist_toggle
 
 urlpatterns = [
     path('watchlist_toggle', watchlist_toggle, name='watchlist_toggle'),
     path(
         'watchlist_changelist/<str:app_label>/<str:model_name>',
         watchlist_changelist, name='watchlist_changelist'
-    ),
-    path('watchlist', WatchlistView.as_view(), name='watchlist'),
+    )
 ]
