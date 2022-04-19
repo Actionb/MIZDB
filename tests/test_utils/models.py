805a1fae09336552507cad5f9d0350cf36d37c61
Actionb <yummytea1@gmail.com>
2022-04-19 10:20:44 +0200

    Moved all models for test_utils into the test modules

--- tests/test_utils/models.py
+++ tests/test_utils/models.py
@@ -1,17 +1,1 @@
-from django.db import models
-
-
-class M2MTarget(models.Model):
-    pass
-
-
-class M2MSource(models.Model):
-    targets = models.ManyToManyField('utils.M2MTarget', related_name='sources')
-
-
-class Protector(models.Model):
-    date = models.DateField(blank=True, null=True)
-
-
-class Protected(models.Model):
-    protector = models.ForeignKey('utils.Protector', on_delete=models.PROTECT)
+# models.py needed to mark this package as an app.
