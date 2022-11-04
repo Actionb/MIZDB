ab1371e35637303494e68e6898d792fe0c891c52
Actionb <yummytea1@gmail.com>
2022-11-04 12:27:54 +0100

    Added tests for commands.updatenames

--- tests/settings.py
+++ tests/settings.py
@@ -30,6 +30,7 @@ TEST_APPS = [
     "tests.test_actions",
     "tests.test_autocomplete",
     "tests.test_base",
+    "tests.test_commands",
     "tests.test_dbentry",
     "tests.test_factory",
     "tests.test_maint",
---
+++ tests/test_commands/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from dbentry.base.models import ComputedNameModel
+
+
+class UpdateCNModel(ComputedNameModel):
+    name_composing_fields = ('_name',)  # use any field to suppress warnings from checks
+
+
+class UpdateNormalModel(models.Model):
+    pass
