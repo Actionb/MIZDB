efdf93a27ed0ed32a3491fa77ff5169bc5f25297
Actionb <yummytea1@gmail.com>
2023-03-16 12:13:41 +0100

    Expand index.html

--- dbentry/site/views.py
+++ dbentry/site/views.py
@@ -8,10 +8,28 @@ class LoginView(auth_views.LoginView):
     next_page = reverse_lazy("mizdb:index")
 
 
-class Index(TemplateView):
-    template_name = "mizdb/index.html"
+class MIZContextMixin:
+    """A mixin that provides context shared by all views."""
+    title = ""
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['has_permission'] = True
+        ctx['title'] = self.title
+        ctx['wiki_url'] = 'http://serv01/wiki/Hauptseite'
+        return ctx
+
+
+class Index(MIZContextMixin, TemplateView):
+    title = "Index"
+
+    template_name = "mizdb/index.html"
+
+    def get_context_data(self, **kwargs):
+        from dbentry import models as _models
+        ctx = super().get_context_data(**kwargs)
+        ctx['model_groups'] = [
+            ('Archivgut', [_models.Ausgabe._meta, _models.Artikel._meta]),
+            ('Stammdaten', [_models.Band._meta, _models.Musiker._meta, _models.Magazin._meta]),
+            ('Sonstige', [_models.Lagerort._meta])
+        ]
         return ctx
