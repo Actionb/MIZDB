52772620cdb2d287f6085ee1f8ee5ae074ad27e9
Actionb <yummytea1@gmail.com>
2023-03-16 12:12:45 +0100

    Expand base.html

--- dbentry/site/templatetags/mizdb.py
+++ dbentry/site/templatetags/mizdb.py
@@ -1,8 +1,16 @@
 from django import template
+from django.contrib.auth import get_permission_codename
 
 register = template.Library()
 
 
-@register.simple_tag
+@register.filter
 def urlname(opts, arg):
     return f"mizdb:{opts.app_label}_{opts.model_name}_{arg}"
+
+
+@register.simple_tag
+def has_perm(user, action, opts):
+    """Return True if the given user has a certain permission to an object."""
+    codename = get_permission_codename(action, opts)
+    return user.has_perm("%s.%s" % (opts.app_label, codename))
