9f6b0ced52ba2d65f7b2b5248d0f8aecb0f21dcb
Actionb <yummytea1@gmail.com>
2023-03-27 11:14:05 +0200

    Rework urlname template tag
    Made it a tag (instead of a filter) and swapped arguments around, making second arg (opts) optional.

--- dbentry/site/templatetags/mizdb.py
+++ dbentry/site/templatetags/mizdb.py
@@ -1,12 +1,21 @@
 from django import template
+from django.conf import settings
 from django.contrib.auth import get_permission_codename
 
 register = template.Library()
 
 
-@register.filter
-def urlname(opts, arg):
-    return f"mizdb:{opts.app_label}_{opts.model_name}_{arg}"
+@register.simple_tag
+def urlname(name, opts=None):
+    """
+    Return the 'url name' for the given name in the current namespace.
+
+    If passing the second argument `opts`, prepend app_label and model_name to
+    the name.
+    """
+    if opts:
+        name = f"{opts.app_label}_{opts.model_name}_{name}"
+    return f"{settings.SITE_NAMESPACE}:{name}"
 
 
 @register.simple_tag
