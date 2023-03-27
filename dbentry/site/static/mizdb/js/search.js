41b05177ab19fe8835cd8d174556835d066ce37f
Actionb <yummytea1@gmail.com>
2023-03-27 11:14:29 +0200

    Pass searchbar search URL to search.js

--- dbentry/site/static/mizdb/js/search.js
+++ dbentry/site/static/mizdb/js/search.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", (event) => {
     const searchInput = document.querySelector('#searchInput');
+    const searchURL = searchInput.dataset.searchUrl;
     const searchResults = document.querySelector('#searchResults');
 
     searchInput.addEventListener("input", (event) => {
         function addResults() {
-            fetch(`http://127.0.0.1:8000/searchbar/?q=${event.target.value}`)
+            fetch(`${searchURL}?q=${event.target.value}`)
                 .then((response) => response.json())
                 .then((data) => {
-                    // FIXME: search is too slow - caching?
                     searchResults.innerHTML = "";
                     const result_list = document.createElement("ul");
                     for (link_html of data.results) {
@@ -18,7 +18,7 @@ window.addEventListener("load", (event) => {
                     searchResults.appendChild(result_list)
                 })
         }
-        
+
         window.clearTimeout(this.delay);
         this.delay = window.setTimeout(addResults, 250);
     });
