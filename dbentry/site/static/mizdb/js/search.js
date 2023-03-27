16012f8fcb5ce18302c58cfb5c9ef2e908aa8119
Actionb <yummytea1@gmail.com>
2023-03-27 10:34:35 +0200

    Add search delay on search modal

--- dbentry/site/static/mizdb/js/search.js
+++ dbentry/site/static/mizdb/js/search.js
@@ -2,20 +2,24 @@ window.addEventListener("load", (event) => {
     const searchInput = document.querySelector('#searchInput');
     const searchResults = document.querySelector('#searchResults');
 
-    // TODO: "keydown" might be the wrong trigger for this? (triggers one keypress too late?)
-    searchInput.addEventListener("keydown", (event) => {
-        fetch(`http://127.0.0.1:8000/searchbar/?q=${event.target.value}`)
-            .then((response) => response.json())
-            .then((data) => {
-                // FIXME: search is too slow - caching?
-                searchResults.innerHTML = "";
-                const result_list = document.createElement("ul");
-                for (link_html of data.results) {
-                    const result_item = document.createElement("li");
-                    result_item.innerHTML = link_html;
-                    result_list.appendChild(result_item);
-                }
-                searchResults.appendChild(result_list)
-            })
+    searchInput.addEventListener("input", (event) => {
+        function addResults() {
+            fetch(`http://127.0.0.1:8000/searchbar/?q=${event.target.value}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    // FIXME: search is too slow - caching?
+                    searchResults.innerHTML = "";
+                    const result_list = document.createElement("ul");
+                    for (link_html of data.results) {
+                        const result_item = document.createElement("li");
+                        result_item.innerHTML = link_html;
+                        result_list.appendChild(result_item);
+                    }
+                    searchResults.appendChild(result_list)
+                })
+        }
+        
+        window.clearTimeout(this.delay);
+        this.delay = window.setTimeout(addResults, 250);
     });
 });
