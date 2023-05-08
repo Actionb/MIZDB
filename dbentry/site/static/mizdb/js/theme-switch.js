34a462b2e21b52f2f64503ac69b740297c4efdeb
Actionb <yummytea1@gmail.com>
2023-05-08 10:25:23 +0200

    load themes directly from bootswatch

--- dbentry/site/static/mizdb/js/theme-switch.js
+++ dbentry/site/static/mizdb/js/theme-switch.js
@@ -1,14 +1,57 @@
-function set_theme(theme_name) {
-    const link = document.getElementById("theme") ;
-    link.setAttribute("href", `https://cdn.jsdelivr.net/npm/bootswatch@5.2.3/dist/${theme_name}/bootstrap.min.css`);
+/* Set a new theme. */
+function set_theme(theme_url) {
+    const link = document.getElementById("user_theme") ;
+    link.setAttribute("href", theme_url);
+    localStorage.theme_url = theme_url;
+}
+
+/* Load the user's current theme. */
+// TODO: move into template as a self-executing function
+function load_initial_theme() {
+    const theme_url = localStorage.theme_url
+    if (theme_url) {
+        set_theme(theme_url);
+    }
+}
+
+/* Set the currently selected theme's dropdown item to active. */
+function set_active_theme(elem) {
+    if (elem === undefined) {
+        const theme_url = document.getElementById("user_theme").getAttribute("href");
+        elem = document.querySelector(`#theme-container a[rel="${theme_url}"`);
+    }
+    if (elem) {
+        document.querySelectorAll("#theme-container a").forEach((elem) => elem.classList.remove("active"));
+        elem.classList.add("active");
+    }
 }
 
 addEventListener("DOMContentLoaded", (event) => {
-    document.querySelectorAll(".change-style-menu-item").forEach((elem) => {
-        elem.addEventListener("click", (event) => {
-            event.preventDefault();
-            var theme_name = event.target.getAttribute('rel');
-            set_theme(theme_name)
+    /* Load available themes from the bootswatch API and create dropdown items for
+     each theme. */
+    fetch("https://bootswatch.com/api/5.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const container = document.getElementById("theme-container");
+            for (var theme of data.themes) {
+                const listItem = document.createElement("li");
+                const switchBtn = document.createElement("a");
+                switchBtn.setAttribute("rel", theme.cssCdn);
+                switchBtn.setAttribute("href", "#");
+                switchBtn.setAttribute("class", "change-style-menu-item dropdown-item");
+                switchBtn.innerText = theme.name;
+                listItem.appendChild(switchBtn);
+                container.appendChild(listItem);
+            }
         });
+
+    set_active_theme();  // for the initial theme
+
+    // Add click event listeners for all theme buttons in the dropdown
+    document.getElementById("theme-container").addEventListener("click", (event) => {
+        event.preventDefault();
+        const elem = event.target;
+        set_theme(elem.getAttribute('rel'));
+        set_active_theme(elem);
     });
 });
