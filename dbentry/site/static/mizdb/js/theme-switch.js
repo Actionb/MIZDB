03514be2adf982aaf71aa11dde305ee07ebe3e10
Actionb <yummytea1@gmail.com>
2023-05-09 11:51:08 +0200

    Some more tweaks

--- dbentry/site/static/mizdb/js/theme-switch.js
+++ dbentry/site/static/mizdb/js/theme-switch.js
@@ -1,57 +1,55 @@
 /* Set a new theme. */
 function set_theme(theme_url) {
-    const link = document.getElementById("user_theme") ;
-    link.setAttribute("href", theme_url);
-    localStorage.theme_url = theme_url;
-}
-
-/* Load the user's current theme. */
-// TODO: move into template as a self-executing function
-function load_initial_theme() {
-    const theme_url = localStorage.theme_url
-    if (theme_url) {
-        set_theme(theme_url);
-    }
+    const link = document.getElementById("user_theme");
+    if (link) {
+        link.setAttribute("href", theme_url);
+        localStorage.setItem("mizdb_theme_url", theme_url);
+   }
 }
 
 /* Set the currently selected theme's dropdown item to active. */
 function set_active_theme(elem) {
-    if (elem === undefined) {
-        const theme_url = document.getElementById("user_theme").getAttribute("href");
-        elem = document.querySelector(`#theme-container a[rel="${theme_url}"`);
-    }
     if (elem) {
-        document.querySelectorAll("#theme-container a").forEach((elem) => elem.classList.remove("active"));
+        document.querySelectorAll("#theme-dropdown a").forEach((elem) => elem.classList.remove("active"));
         elem.classList.add("active");
     }
 }
 
 addEventListener("DOMContentLoaded", (event) => {
-    /* Load available themes from the bootswatch API and create dropdown items for
-     each theme. */
-    fetch("https://bootswatch.com/api/5.json")
-        .then((response) => response.json())
-        .then((data) => {
-            const container = document.getElementById("theme-container");
-            for (var theme of data.themes) {
-                const listItem = document.createElement("li");
-                const switchBtn = document.createElement("a");
-                switchBtn.setAttribute("rel", theme.cssCdn);
-                switchBtn.setAttribute("href", "#");
-                switchBtn.setAttribute("class", "change-style-menu-item dropdown-item");
-                switchBtn.innerText = theme.name;
-                listItem.appendChild(switchBtn);
-                container.appendChild(listItem);
-            }
+    const dropdown = document.getElementById("theme-dropdown");
+    const link = document.getElementById("user_theme");
+    // Load additional themes from the bootswatch API and add them to the theme dropdown.
+    if (dropdown && link) {
+        fetch("https://bootswatch.com/api/5.json")
+            .then((response) => response.json())
+            .then((data) => {
+                for (var theme of data.themes) {
+                    const listItem = document.createElement("li");
+                    const switchBtn = document.createElement("a");
+                    switchBtn.setAttribute("rel", theme.cssCdn);
+                    switchBtn.setAttribute("href", "#");
+                    switchBtn.setAttribute("class", "change-theme-menu-item dropdown-item");
+                    switchBtn.setAttribute("crossorigin", "anonymous");
+                    switchBtn.innerText = theme.name;
+                    listItem.appendChild(switchBtn);
+                    dropdown.appendChild(listItem);
+                }
+            });
+
+        // Add click event listener for the theme dropdown items.
+        dropdown.addEventListener("click", (event) => {
+            event.preventDefault();
+            const elem = event.target;
+            set_theme(elem.getAttribute('rel'));
+            set_active_theme(elem);
         });
-
-    set_active_theme();  // for the initial theme
-
-    // Add click event listeners for all theme buttons in the dropdown
-    document.getElementById("theme-container").addEventListener("click", (event) => {
-        event.preventDefault();
-        const elem = event.target;
-        set_theme(elem.getAttribute('rel'));
+    
+        // Set the menu item of the initial theme to active.
+        const theme_url = link.getAttribute("href");
+        const elem = document.querySelector(`#theme-dropdown a[rel="${theme_url}"`);
         set_active_theme(elem);
-    });
+    }
 });
+
+/* Load the user's current theme. */
+(() => set_theme(localStorage.getItem("mizdb_theme_url")))();
