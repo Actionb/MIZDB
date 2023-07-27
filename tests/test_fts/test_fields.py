19c6ed509d3aa7ff6f0c715887df536c2544bd64
Actionb <yummytea1@gmail.com>
2023-07-27 13:54:36 +0200

    Update tests

--- tests/test_fts/test_fields.py
+++ tests/test_fts/test_fields.py
@@ -4,48 +4,45 @@ from django.test import TestCase
 from dbentry.fts.fields import SearchVectorField, WeightedColumn
 
 
-class TestWeightedColumn(TestCase):
+column = WeightedColumn("title", "A", "simple")
 
+
+class SearchVectorModel(models.Model):
+    title = models.CharField(max_length=100)
+    search_field = SearchVectorField(columns=[column])
+
+
+class TestWeightedColumn(TestCase):
     def test_weighted_column_deconstruct(self):
         """
         Assert that deconstruct returns the correct path and arguments for
         WeightedColumn.
         """
-        column = WeightedColumn('titel', 'A', language='simple')
-        path, args, kwargs = column.deconstruct()
-        self.assertEqual(path, 'dbentry.fts.fields.WeightedColumn')
-        self.assertEqual(args, ['titel', 'A', 'simple'])
+        path, args, _kwargs = WeightedColumn("titel", weight="A", config="simple").deconstruct()
+        self.assertEqual(path, "dbentry.fts.fields.WeightedColumn")
+        self.assertEqual(args, ["titel", "A", "simple"])
 
 
 class TestSearchVectorField(TestCase):
+    def test_init(self):
+        """
+        A SearchVectorField should always initialize with null=True, blank=True
+        and editable=False.
+        """
+        f = SearchVectorField()
+        self.assertTrue(f.null)
+        self.assertTrue(f.blank)
+        self.assertFalse(f.editable)
 
     def test_search_field_deconstruct(self):
         """
         Assert that deconstruct returns the correct path and arguments for
         SearchVectorField.
         """
-        column = WeightedColumn('title', 'A', 'simple')
-        field = SearchVectorField(
-            columns=[column],
-            blank=False, editable=True,  # inverted default arguments
-        )
-
-        class SearchVectorModel(models.Model):
-            title = models.CharField(max_length=100)
-            search_field = field
-
-        _name, path, args, kwargs = SearchVectorModel._meta.get_field('search_field').deconstruct()
-        self.assertEqual(_name, 'search_field')
-        self.assertEqual(path, 'dbentry.fts.fields.SearchVectorField')
-        self.assertFalse(args)
-        self.assertEqual(kwargs['columns'], [column])
-        self.assertFalse(kwargs['blank'])
-        self.assertTrue(kwargs['editable'])
-
-        # blank and editable should be omitted if the parameters are the
-        # default values.
-        field = SearchVectorField(columns=[column], blank=True, editable=False)
-        _name, _path, _args, kwargs = field.deconstruct()
-        self.assertEqual(path, 'dbentry.fts.fields.SearchVectorField')
-        self.assertNotIn('blank', kwargs)
-        self.assertNotIn('editable', kwargs)
+        name, path, _args, kwargs = SearchVectorModel._meta.get_field("search_field").deconstruct()
+        self.assertEqual(name, "search_field")
+        self.assertEqual(path, "dbentry.fts.fields.SearchVectorField")
+        self.assertEqual(kwargs["columns"], [column])
+        self.assertTrue(kwargs["null"])
+        self.assertTrue(kwargs["blank"])
+        self.assertFalse(kwargs["editable"])
