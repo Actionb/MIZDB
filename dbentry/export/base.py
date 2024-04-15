c53516fdf8a35f850985ab5a03425e6e5bda9b1e
Actionb <yummytea1@gmail.com>
2024-04-15 14:26:03 +0200

    Forgot the model in the as_string meta

--- dbentry/export/base.py
+++ dbentry/export/base.py
@@ -150,17 +150,26 @@ class MIZResource(ModelResource):
     def as_string(self):
         indent = " " * 4
         r = f"class {self.__class__.__name__}(MIZResource):\n"
+
+        # Stringify the annotation fields:
         if self._meta._declared_fields:
             for name, field in self._meta._declared_fields.items():
                 r += f'{indent}{name} = Field(attribute="{field.attribute}", column_name="{field.column_name}")\n'
             r += "\n"
+
+        # Stringify the Meta class:
         r += f"{indent}class Meta:\n"
         indent *= 2
+
+        r += f"{indent}model = _models.{self._meta.model.__name__}\n"
+
         fields = str(self._meta.fields).replace("'", '"')
-        r += f'{indent}fields = {fields}\n{indent}export_order = {fields}\n'
+        r += f"{indent}fields = {fields}\n{indent}export_order = {fields}\n"
+
         if self._meta.annotations:
             _annotations = OrderedDict()
             for name, expr in self._meta.annotations.items():
+                # Inspect the expression to recover the original arguments:
                 # string_list expressions: limit(array_to_string(array_agg(...)))
                 array_to_string = expr.source_expressions[0]
                 array_agg, sep_expr, default_expr = array_to_string.source_expressions
@@ -171,11 +180,13 @@ class MIZResource(ModelResource):
                 else:
                     annotation = f'string_list("{path}")'
                 _annotations[name] = annotation
-            _annotations = ', '.join(f'"{name}": {expr}' for name, expr in _annotations.items())
+            _annotations = ", ".join(f'"{name}": {expr}' for name, expr in _annotations.items())
             r += f"{indent}annotations = {{{_annotations}}}\n"
+
         if self._meta.widgets:
             widgets = str(self._meta.widgets).replace("'", '"')
             r += f"{indent}widgets = {widgets}\n"
+
         return r
 
 
