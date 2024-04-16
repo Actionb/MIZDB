7d1f32df084f8760d2a3471edf2755baf388fa51
Actionb <yummytea1@gmail.com>
2024-04-16 12:34:16 +0200

    Always defer fts field

--- dbentry/export/base.py
+++ dbentry/export/base.py
@@ -45,6 +45,7 @@ class MIZResource(ModelResource):
             return queryset
 
     def filter_export(self, queryset, *args, **kwargs):
+        queryset = queryset.defer("_fts")
         queryset = self._add_annotations(queryset)
         if select_related := getattr(self._meta, "select_related", None):
             queryset = queryset.select_related(*select_related)
