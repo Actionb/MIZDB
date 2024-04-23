94583f190e1adf56cb0e171421933b03291dc778
Actionb <yummytea1@gmail.com>
2024-04-23 12:13:03 +0200

    Add ChoiceLabelWidget

--- dbentry/export/widgets.py
+++ dbentry/export/widgets.py
@@ -1,4 +1,4 @@
-from import_export.widgets import BooleanWidget
+from import_export.widgets import BooleanWidget, CharWidget
 
 
 class YesNoBooleanWidget(BooleanWidget):
@@ -6,3 +6,18 @@ class YesNoBooleanWidget(BooleanWidget):
         if value in self.NULL_VALUES:
             return ""
         return "Ja" if value else "Nein"
+
+
+class ChoiceLabelWidget(CharWidget):
+    """A widget that exports the human-readable label of the selected choice."""
+
+    def __init__(self, field_name):
+        self.field_name = field_name
+        super().__init__()
+
+    def render(self, value, obj=None):
+        try:
+            return getattr(obj, f"get_{self.field_name}_display")()
+        except (AttributeError, TypeError):
+            pass
+        return super().render(value, obj=obj)
