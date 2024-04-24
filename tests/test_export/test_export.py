da4265a11ab92fea19d4a3d63644a9a4805b7759
Actionb <yummytea1@gmail.com>
2024-04-24 10:12:13 +0200

    Update tests

--- tests/test_export/test_export.py
+++ tests/test_export/test_export.py
@@ -89,7 +89,6 @@ def sort(_list):
 
 
 def test_can_export(dataset):
-    # FIXME: _models.Musiker.objects.count() is '5' here (musiker + four unknowns)?
     assert len(dataset) == 1
 
 
@@ -106,17 +105,6 @@ def test_headers(dataset):
         "Instrumente",
         "Beschreibung",
     ]
-
-
-def test_annotations(resource):
-    annotations = resource.get_annotations()
-    assert annotations
-    assert "urls_list" in annotations
-    assert "genre_list" in annotations
-    assert "musikeralias_list" in annotations
-    assert "band_list" not in annotations
-    assert "orte_list" in annotations
-    assert "instrument_list" in annotations
 
 
 def test_queryset_select_related(resource):
