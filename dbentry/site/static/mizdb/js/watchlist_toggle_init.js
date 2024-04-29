a71fe308eb5cc166160b0b0fa9506437a267a6f2
Actionb <yummytea1@gmail.com>
2024-04-29 14:23:28 +0200

    Add watchlist_toggle_init.js

---
+++ dbentry/site/static/mizdb/js/watchlist_toggle_init.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.watchlist-toggle-btn')
+    const callback = (btn, data) => {
+        if (!data) return
+        if (data.on_watchlist) {
+            btn.classList.remove('btn-primary')
+            btn.classList.add('btn-success')
+        } else {
+            btn.classList.add('btn-primary')
+            btn.classList.remove('btn-success')
+        }
+    }
+    WatchlistButton.initToggleButton(toggleButton, callback)
+})
