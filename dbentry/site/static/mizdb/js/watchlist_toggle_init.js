4e5b40f96016b040e099d6cc2717f1cde2ada83a
Actionb <yummytea1@gmail.com>
2024-05-02 11:14:20 +0200

    Check if watchlist toggle button exist before init

--- dbentry/site/static/mizdb/js/watchlist_toggle_init.js
+++ dbentry/site/static/mizdb/js/watchlist_toggle_init.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.watchlist-toggle-btn')
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".watchlist-toggle-btn");
+  if (toggleButton) {
     const callback = (btn, data) => {
-        if (!data) return
-        if (data.on_watchlist) {
-            btn.classList.remove('btn-primary')
-            btn.classList.add('btn-success')
-        } else {
-            btn.classList.add('btn-primary')
-            btn.classList.remove('btn-success')
-        }
-    }
-    WatchlistButton.initToggleButton(toggleButton, callback)
-})
+      if (!data) return;
+      if (data.on_watchlist) {
+        btn.classList.remove("btn-primary");
+        btn.classList.add("btn-success");
+      } else {
+        btn.classList.add("btn-primary");
+        btn.classList.remove("btn-success");
+      }
+    };
+    WatchlistButton.initToggleButton(toggleButton, callback);
+  }
+});
