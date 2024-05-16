15910a7bfbd2251950ff016a371502e56ec017a6
Actionb <yummytea1@gmail.com>
2024-05-16 10:54:14 +0200

    Tweak parser output

--- dbentry/help/wiki_parser/parser.py
+++ dbentry/help/wiki_parser/parser.py
@@ -95,24 +95,25 @@ class WikiParser:
     def as_html(self):
         # TODO: add proper <head>
         template = textwrap.dedent(
-            """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <title></title>
-                    <meta name="description" content="">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-                    <link id="default_theme" href="/static/mizdb/css/mizdb_theme.css" rel="stylesheet">
-                </head>
-                <body class="container">%s</body>
-            </html>
+            """<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link id="default_theme" href="/static/mizdb/css/mizdb_theme.css" rel="stylesheet">
+    </head>
+    <body class="container">
+        %s
+    </body>
+</html>
             """
         )
-        return template % "\n".join(str(t) for t in self.elements if t is not None)
+        return template % f"\n{' ' * 8}".join(str(t) for t in self.elements if t is not None)
 
 
 if __name__ == '__main__':
