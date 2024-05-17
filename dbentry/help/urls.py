5b391ed5632d7949f89a4ccbadc213a4af4621b4
Actionb <yummytea1@gmail.com>
2024-05-17 09:58:49 +0200

    Tweak help urls

--- dbentry/help/urls.py
+++ dbentry/help/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from dbentry.help.views import HelpView, HelpIndex
 
 urlpatterns = [
-    path("index", HelpIndex.as_view(), name="help_index"),
-    path("<str:title>/", HelpView.as_view(), name="help")
+    path("", HelpIndex.as_view(), name="help_index"),
+    path("<str:title>/", HelpView.as_view(), name="help"),
 ]
