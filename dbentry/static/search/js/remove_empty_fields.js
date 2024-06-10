8f023524afa6df0ba237c2013933c63af9afccdf
Actionb <yummytea1@gmail.com>
2024-06-10 12:04:37 +0200

    remove_empty_fields: check if form exists

--- dbentry/static/search/js/remove_empty_fields.js
+++ dbentry/static/search/js/remove_empty_fields.js
@@ -1,10 +1,12 @@
 // Remove empty form data from search forms
 document.addEventListener("DOMContentLoaded", () => {
 	const form = document.querySelector(".search-form")
-	form.addEventListener("formdata", (e) => {
-	    const formData = e.formData
-	    for (const [name, value] of [...formData.entries()]){
-	        if (value === "") formData.delete(name)
-	    }
-	})
+	if (form) {
+        form.addEventListener("formdata", (e) => {
+            const formData = e.formData
+            for (const [name, value] of [...formData.entries()]){
+                if (value === "") formData.delete(name)
+            }
+        })
+    }
 })
