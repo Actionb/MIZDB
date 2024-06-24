3b7801a873140b9ac20d3f6fcb5edf9e01a70a8e
Actionb <yummytea1@gmail.com>
2024-06-24 12:58:20 +0200

    logging: add rotating file handler

--- MIZDB/settings/production.py
+++ MIZDB/settings/production.py
@@ -1,5 +1,9 @@
 from .defaults import *  # noqa
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR/MIZDB project dir/settings dir/__file__
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 try:
     with open(BASE_DIR / ".secrets" / ".allowedhosts") as f:
         ALLOWED_HOSTS = f.readline().strip().split(",")
@@ -12,7 +16,7 @@ except FileNotFoundError as e:
     ) from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # URL to the wiki.
 # That URL is displayed in the header on each admin page.
@@ -25,6 +29,15 @@ LOGGING = {
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+        },
+        "timed_rotating_file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "level": os.getenv("MIZDB_LOG_LEVEL", "INFO"),
+            "filename": Path(os.getenv("LOG_DIR", BASE_DIR)) / "mizdb.log",
+            "formatter": "default",
+            "delay": True,
+            "when": "W0",  # rollover every Monday (Weekday 0)
+            "backupCount": 12,  # keep 12 weeks worth of logs
         },
     },
     "formatters": {
