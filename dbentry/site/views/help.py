ee5ede10490a3184197a2994f582c66805ff2fa8
Actionb <yummytea1@gmail.com>
2024-07-02 12:06:43 +0200

    404 if help template does not exist

--- dbentry/site/views/help.py
+++ dbentry/site/views/help.py
@@ -1,10 +1,20 @@
+from django.http import Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
 from django.views.generic import TemplateView
 
 from dbentry.site.views import BaseViewMixin
 
 
 class HelpView(BaseViewMixin, TemplateView):
-    page_name: str
+
+    def get(self, request, *args, **kwargs):
+        try:
+            get_template(self.get_template_names()[0])
+        except TemplateDoesNotExist:
+            raise Http404
+        else:
+            return super().get(request, *args, **kwargs)
 
     def get_template_names(self):
         return [f'help/{self.kwargs["page_name"]}.html']
