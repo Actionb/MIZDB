41da34843e2225bb4754c6790ab132d6111a782c
Actionb <yummytea1@gmail.com>
2024-07-15 11:40:17 +0200

    Only apply styles inside the help_content

--- docs/js/style.js
+++ docs/js/style.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const help_content = document.querySelector('#help_content')
+    if (!help_content) return
+    
     // Style the tables
-    document.querySelectorAll('table').forEach(table => table.classList.add('table', 'table-striped', 'table-hover'))
+    help_content.querySelectorAll('table').forEach(table => table.classList.add('table', 'table-striped', 'table-hover'))
 
     // Style the headings
-    document.querySelectorAll('h1,h2,h3,h4,h5').forEach(heading => heading.classList.add('text-info-emphasis'))
-    document.querySelectorAll('h2').forEach(heading => heading.classList.add('fs-3', 'mb-3'))
-    document.querySelectorAll("h3 ~ h2").forEach(h => h.classList.add('pt-3'))
-    document.querySelectorAll('h3').forEach(heading => heading.classList.add('fs-5'))
-    document.querySelectorAll('h3:not(h2+h3)').forEach(heading => heading.classList.add('fs-5', 'pt-3', 'border-top'))
+    help_content.querySelectorAll('h1,h2,h3,h4,h5').forEach(heading => heading.classList.add('text-info-emphasis'))
+    help_content.querySelectorAll('h2').forEach(heading => heading.classList.add('fs-3', 'mb-3'))
+    help_content.querySelectorAll('h3 ~ h2').forEach(h => h.classList.add('pt-3'))
+    help_content.querySelectorAll('h3').forEach(heading => heading.classList.add('fs-5'))
+    help_content.querySelectorAll('h3:not(h2+h3)').forEach(heading => heading.classList.add('fs-5', 'pt-3', 'border-top'))
 
     // Indent the paragraphs
-    document.querySelectorAll('[role=main] p').forEach(p => p.classList.add('ps-3'))
+    help_content.querySelectorAll('[role=main] p').forEach(p => p.classList.add('ps-3'))
 
     // Hide the toc if it only contains one item
-    const toc = document.querySelector('#help_content .bs-sidebar')
+    const toc = help_content.querySelector('.bs-sidebar')
     if (toc && toc.querySelectorAll('.nav-item').length == 1) {
         toc.classList.add('d-none')
     }
