8b774110e65f9d365f67ba94287b80522d03da0f
Actionb <yummytea1@gmail.com>
2024-08-22 12:17:51 +0200

    watchlist overview: include magazin name in ausgabe watchlist items

--- dbentry/site/views/watchlist.py
+++ dbentry/site/views/watchlist.py
@@ -1,9 +1,16 @@
 from django.views.generic import TemplateView
 from mizdb_watchlist.views import WatchlistViewMixin
 
+from dbentry.models import Ausgabe
 from dbentry.site.views import BaseViewMixin
 
 
 class WatchlistView(WatchlistViewMixin, BaseViewMixin, TemplateView):
     template_name = "mizdb/watchlist.html"
     title = "Merkliste"
+
+    def get_object_text(self, request, model, pk, object_repr=""):
+        if model == Ausgabe:
+            obj = model.objects.select_related("magazin").get(pk=pk)
+            return f"{str(obj)} ({str(obj.magazin)})"
+        return super().get_object_text(request, model, pk, object_repr)
