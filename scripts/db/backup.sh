e3571fac6e21b1c10c47b5f85520ba387ec8558d
Actionb <yummytea1@gmail.com>
2025-06-13 15:49:52 +0200

    Manage backups outside of the db container

--- scripts/db/backup.sh
+++ scripts/db/backup.sh
@@ -3,14 +3,17 @@
 #
 # Use this in a cronjob (on the host machine of the docker container):
 # > crontab -e
-# > 51 7,11,16 * * 1-5  docker exec mizdb-postgres sh /mizdb/backup.sh
+# > 51 7,11,16 * * 1-5  /path/to/this/backup.sh
 
+# The directory where the backups should go:
 BACKUP_DIR="/var/lib/mizdb/backups"
-# Numbers of days you want to keep copies of your database:
+# The name of the db container:
+db_container=mizdb-postgres
+# Numbers of days you want to keep copies of the database dumps:
 number_of_days=30
 
 file="${BACKUP_DIR}/mizdb_$(date +%Y_%m_%d_%H_%M_%S)"
-pg_dump --username="$POSTGRES_USER" --host=localhost -Fc "$POSTGRES_DB" > "$file"
+docker exec -i "$db_container" /bin/sh -c 'pg_dump --username="$POSTGRES_USER" --host=localhost -Fc "$POSTGRES_DB"' > "$file"
 
 # Delete older backup copies:
 find "$BACKUP_DIR" -name "mizdb_*" -type f -mtime +$number_of_days -delete
