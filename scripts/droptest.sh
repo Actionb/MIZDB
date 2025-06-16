6b8736c9eeb3dd121926ffd74908d0e2ffdfe7a5
Actionb <yummytea1@gmail.com>
2025-06-16 17:23:26 +0200

    droptest.sh: use DB_PASSWORD env var

--- scripts/droptest.sh
+++ scripts/droptest.sh
@@ -2,11 +2,8 @@
 
 # Drops all test databases (i.e. databases starting with the string "test_mizdb").
 
-# Read password from the password file
-PASSWORD=$(cat ".passwd")
-
 echo "This drops the following databases:"
-PGPASSWORD="$PASSWORD" psql --user=mizdb_user --host=localhost --dbname=mizdb -c "SELECT datname FROM pg_database WHERE datname LIKE 'test_mizdb%';" | \
+PGPASSWORD="$DB_PASSWORD" psql --user=mizdb_user --host=localhost --dbname=mizdb -c "SELECT datname FROM pg_database WHERE datname LIKE 'test_mizdb%';" | \
   grep -ve "datname" -e "^-" -e "^("
 read -r -p "Continue? [y/n]: "
 if [[ ! $REPLY =~ ^[jJyY]$ ]]; then
@@ -14,6 +11,6 @@ if [[ ! $REPLY =~ ^[jJyY]$ ]]; then
   exit 0
 fi
 
-PGPASSWORD="$PASSWORD" psql --user=mizdb_user --host=localhost --dbname=mizdb -c "SELECT datname FROM pg_database WHERE datname LIKE 'test_mizdb%';" | \
+PGPASSWORD="$DB_PASSWORD" psql --user=mizdb_user --host=localhost --dbname=mizdb -c "SELECT datname FROM pg_database WHERE datname LIKE 'test_mizdb%';" | \
   grep -ve "datname" -e "^-" -e "^(" | \
-  PGPASSWORD="$PASSWORD" xargs -I {} psql --user=mizdb_user --host=localhost --dbname=mizdb -c "DROP DATABASE {};"
+  PGPASSWORD="$DB_PASSWORD" xargs -I {} psql --user=mizdb_user --host=localhost --dbname=mizdb -c "DROP DATABASE {};"
