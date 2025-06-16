d085dc8d1e8d139dc27fb62010e2eb672547087c
Actionb <yummytea1@gmail.com>
2025-06-16 13:55:07 +0200

    Tweak install script

--- scripts/install-mizdb.sh
+++ scripts/install-mizdb.sh
@@ -1,79 +1,78 @@
 #!/usr/bin/env bash
-# Install MIZDB.
-#
-# Inspired by the install script from the awesome paperless-ngx project!
+# Installiere MIZDB in den Unterordner 'MIZDB'.
+
+# With some inspiration from the install script for the awesome paperless-ngx project!
 
 set -e
 
+# Run some checks before starting
 if [[ $(id -u) == "0" ]] ; then
-	echo "Do not run this script as root."
-	exit 1
-fi
-
-if ! command -v wget &> /dev/null ; then
-	echo "wget executable not found. Is wget installed?"
+	echo "Das Skript darf nichts als root ausgeführt werden."
 	exit 1
 fi
 
 if ! command -v docker &> /dev/null ; then
-	echo "docker executable not found. Is docker installed?"
+	echo "docker executable nicht gefunden. Ist docker installiert?"
 	exit 1
 fi
 
 if ! docker compose &> /dev/null ; then
-	echo "docker compose plugin not found. Is docker compose installed?"
+	echo "docker compose plugin nicht gefunden. Ist docker compose installiert?"
 	exit 1
 fi
 
-ask_pw () {
-  printf "Soll für den Datenbankbenutzer ein Passwort eingerichtet werden (y/n)?: "
-  read -r REPLY
-  case "$REPLY" in
-    [!JjYy]*) return 0 ;;
-  esac
-  while true; do
-    read -r -sp "Bitte Passwort für den Datenbankbenutzer eingeben: " PASSWORD
-    echo ""
+# Ask for a password
+printf "Soll für den Datenbankbenutzer ein Passwort eingerichtet werden (y/n)?: "
+read -r REPLY
+case "$REPLY" in
+  [JjYy]*)
+    while true; do
+      read -r -sp "Bitte Passwort für den Datenbankbenutzer eingeben: " PASSWORD
+      echo ""
 
-    if [[ -z $PASSWORD ]] ; then
-      echo "Passwort darf nicht leer sein."
-      continue
-    fi
+      if [[ -z $PASSWORD ]] ; then
+        echo "Passwort darf nicht leer sein."
+        continue
+      fi
 
-    read -r -sp "Bitte das Passwort noch einmal eingeben: " PASSWORD_REPEAT
-    echo ""
+      read -r -sp "Bitte das Passwort noch einmal eingeben: " PASSWORD_REPEAT
+      echo ""
 
-    if [[ ! "$PASSWORD" == "$PASSWORD_REPEAT" ]] ; then
-      echo "Passwörter stimmen nicht überein"
-    else
-      break
-    fi
-  done
-}
+      if [[ ! "$PASSWORD" == "$PASSWORD_REPEAT" ]] ; then
+        echo "Passwörter stimmen nicht überein"
+      else
+        break
+      fi
+    done
+    ;;
+esac
 
-ask_pw
-
+# Prepare the docker files
 # TODO: ask for an install folder?
 mkdir -p MIZDB
 cd MIZDB
 
 echo "Lade MIZDB Dateien herunter..."
-# TODO: set correct URLs
-wget "https://raw.githubusercontent.com/Actionb/MIZDB/refs/heads/feature/docker-image/docker/docker-compose.yaml" -q -O docker-compose.yaml
-wget "https://raw.githubusercontent.com/Actionb/MIZDB/refs/heads/feature/docker-image/docker/docker-compose.env" -q -O docker-compose.env
-wget "https://raw.githubusercontent.com/Actionb/MIZDB/refs/heads/feature/docker-image/mizdb.sh" -q -O mizdb.sh
+# TODO: use production URLs once merged to master branch
+curl -fsSL "https://raw.githubusercontent.com/Actionb/MIZDB/refs/heads/feature/docker-image/docker/docker-compose.yaml" -o docker-compose.yaml
+curl -fsSL "https://raw.githubusercontent.com/Actionb/MIZDB/refs/heads/feature/docker-image/docker/docker-compose.env" -o docker-compose.env
+curl -fsSL "https://raw.githubusercontent.com/Actionb/MIZDB/refs/heads/feature/docker-image/mizdb.sh" -o mizdb.sh
 
 echo "Erzeuge MIZDB Einstellungen..."
 if [[ $PASSWORD ]]; then sed -i "s/DB_PASSWORD=mizdb/DB_PASSWORD=$PASSWORD/" docker-compose.env; fi
 SECRET_KEY=$(LC_ALL=C tr -dc 'a-zA-Z0-9!#$%&()*+,-.:;<=>?@[\]^_`{|}~' < /dev/urandom | dd bs=1 count=64 2>/dev/null)
 sed -i "s/#SECRET_KEY=/SECRET_KEY=$SECRET_KEY/" docker-compose.env
 
+# Start docker containers
 echo "Lade Docker Images herunter..."
 docker compose --env-file docker-compose.env pull
 
 echo "Starte Docker Container..."
 docker compose --env-file docker-compose.env up -d
 
+cd - > /dev/null
+
+# Add mizdb management utility to the user's path
 echo "Erstelle Management Skript..."
 cat << EOF > ~/.local/bin/mizdb
 #!/bin/sh
@@ -85,18 +84,34 @@ cd - > /dev/null || exit
 EOF
 chmod +x ~/.local/bin/mizdb
 
-if [ -f "$1" ]; then
-  echo "Stelle Datenbank aus Backup wieder her..."
-  cmd=$(cat <<'EOF'
+# Restore backup or run migrations
+restore_cmd=$(cat <<'EOF'
 dropdb --username="$POSTGRES_USER" --host=localhost "$POSTGRES_DB" \
 && createdb --username="$POSTGRES_USER" --host=localhost --owner="$POSTGRES_USER" "$POSTGRES_DB" \
 && pg_restore --username="$POSTGRES_USER" --host=localhost --dbname "$POSTGRES_DB"
 EOF
-  )
-  docker exec -i mizdb-postgres /bin/sh -c "$cmd" < "$1"
-else
-  echo "Erzeuge Datenbank Tabellen..."
-  docker exec -i mizdb-app python manage.py migrate
-fi
+)
+
+printf "Soll ein Backup wiederhergestellt werden? (y/n)?: "
+read -r REPLY
+case "$REPLY" in
+  [JjYy]*)
+    while true; do
+      read -r -p "Bitte Pfad zur Backup-Datei angeben (z.B. ~/Downloads/backup oder ./backup): " BACKUP
+      BACKUP="$(readlink -f "$BACKUP")"
+      if [[ -f "$BACKUP" ]]; then
+        break
+      else
+        echo "Datei nicht gefunden! Bitte benutze absolute (also: /pfad/zu/backup ) oder relative Pfade (also: ./backup )."
+      fi
+    done
+    echo "Stelle Datenbank aus Backup wieder her..."
+    docker exec -i mizdb-postgres /bin/sh -c "$restore_cmd" < "$BACKUP"
+    ;;
+  *)
+    echo "Wende Datenbank Migration an..."
+    docker exec -i mizdb-app python manage.py migrate
+    ;;
+esac
 
 echo "Fertig!"
