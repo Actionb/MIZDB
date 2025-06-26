def1131c7437687dc8dd66f56b973be16fb9c8e5
Actionb <yummytea1@gmail.com>
2025-06-26 13:30:37 +0200

    Add get_verbose_name_for_resource_field helper function

--- tests/test_export/test_base.py
+++ tests/test_export/test_base.py
@@ -4,7 +4,7 @@ from django.core.exceptions import FieldDoesNotExist
 from django.db import models
 from import_export.fields import Field
 
-from dbentry.export.base import MIZResource
+from dbentry.export.base import MIZResource, get_verbose_name_for_resource_field
 from dbentry.export.fields import AnnotationField
 from dbentry.export.widgets import YesNoBooleanWidget
 from tests.case import MIZTestCase
@@ -167,3 +167,62 @@ class TestMIZResource(MIZTestCase):
     def test_widget_from_django_field_no_boolean_field(self):
         field = models.IntegerField()
         self.assertNotEqual(MIZResource.widget_from_django_field(field), YesNoBooleanWidget)
+
+
+class TestGetVerboseName(MIZTestCase):
+    def setUp(self):
+        self.resource = DummyResource()
+
+    def test_get_verbose_name_for_resource_field_column_name_set(self):
+        """
+        Assert that get_verbose_name_for_resource_field prioritizes an
+        explicitly set column_name over other names.
+        """
+        field = Field(attribute="name_set", column_name="Explicit Column Name")
+        with patch.object(self.resource, "get_export_fields", new=Mock(return_value=[field])):
+            with patch.object(self.resource, "fields", new={"name_set": field}):
+                self.assertEqual(get_verbose_name_for_resource_field(self.resource, "name_set"), "Explicit Column Name")
+
+    def test_get_verbose_name_for_resource_field_model_verbose_name(self):
+        """
+        Assert that get_verbose_name_for_resource_field uses the verbose name
+        defined on the model field if no explicit column_name is set.
+        """
+        field = Field(attribute="name_set", column_name="name_set")
+        with patch.object(self.resource, "get_export_fields", new=Mock(return_value=[field])):
+            with patch.object(self.resource, "fields", new={"name_set": field}):
+                self.assertEqual(
+                    get_verbose_name_for_resource_field(self.resource, "name_set"), "Verbose Name Explicitly Set"
+                )
+
+    def test_get_verbose_name_for_resource_field_derived_verbose_name(self):
+        """
+        Assert that get_verbose_name_for_resource_field capitalizes the first
+        letter of a model field verbose name that has been derived from the
+        model field's name because no explicit verbose name was set.
+        """
+        field = Field(attribute="no_name_set")
+        with patch.object(self.resource, "get_export_fields", new=Mock(return_value=[field])):
+            with patch.object(self.resource, "fields", new={"no_name_set": field}):
+                self.assertEqual(get_verbose_name_for_resource_field(self.resource, "no_name_set"), "No name set")
+
+    def test_get_verbose_name_for_resource_field_no_model_field_column_name(self):
+        """
+        Assert that get_verbose_name_for_resource_field uses the field's name
+        if no model field could be found and no explicit column name was set.
+        """
+        field = Field(attribute="does_not_exist")
+        with patch.object(self.resource, "get_export_fields", new=Mock(return_value=[field])):
+            with patch.object(self.resource, "fields", new={"does_not_exist": field}):
+                self.assertEqual(get_verbose_name_for_resource_field(self.resource, "does_not_exist"), "Does not exist")
+
+    def test_get_verbose_name_for_resource_field_no_resource_field(self):
+        """
+        Assert that get_verbose_name_for_resource_field raises a ValueError if
+        the resource does not have a field with the given name.
+        """
+
+        def test_callable():
+            return get_verbose_name_for_resource_field(self.resource, "foo")
+
+        self.assertRaises(ValueError, test_callable)
